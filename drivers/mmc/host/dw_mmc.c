adaf3ed7ec2c4ac8143165d440e2af8bf56d1777
Hosung Kim <hosung0.kim@samsung.com>
2012-10-09 14:14:34 +0900

    mmc: dw_mmc: fix bit set of abort command by host controller
    This patch fixes missing bit ctrl when sending abort command.
    When data data error is occurred, host controller issue abort command
    for stopping data transmission. Control of response CRC bit and expect
    response bit was missing when issuing abort command by host controller.
    
    Bug: 7273787
    
    Change-Id: I15f2c1e1f07a859354a23e1124efd9bea28b020c
    Signed-off-by: Hosung Kim <hosung0.kim@samsung.com>

--- drivers/mmc/host/dw_mmc.c
+++ drivers/mmc/host/dw_mmc.c
@@ -458,7 +458,8 @@ static u32 dw_mci_prep_stop(struct dw_mci *host, struct mmc_command *cmd)
 	} else
 		return 0;
 
-	cmdr = stop->opcode | SDMMC_CMD_STOP;
+	cmdr = stop->opcode | SDMMC_CMD_STOP |
+		SDMMC_CMD_RESP_CRC | SDMMC_CMD_RESP_EXP;
 
 	/* Use hold bit register */
 	if (drv_data && drv_data->prepare_command)
