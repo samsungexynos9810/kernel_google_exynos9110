0b9d09f284f354098de46c36fc052ae7109a6785
Donghoon Yu <hoony.yu@samsung.com>
2013-01-10 14:02:34 +0900

    [COMMON] mmc: sd: modify initialization sequence for UHS-I card.
    in case of failure of tuning UHS-I card, tuning again in low clock.
    
    Change-Id: Id69c1e5f7931f257976cea9ac1b75e059c8bc45e
    Signed-off-by: Donghoon Yu <hoony.yu@samsung.com>

--- drivers/mmc/core/sd.c
+++ drivers/mmc/core/sd.c
@@ -646,12 +646,26 @@ static int mmc_sd_init_uhs_card(struct mmc_card *card)
 	if (err)
 		goto out;
 
+try:
 	/* SPI mode doesn't define CMD19 */
 	if (!mmc_host_is_spi(card->host) && card->host->ops->execute_tuning) {
 		mmc_host_clk_hold(card->host);
 		err = card->host->ops->execute_tuning(card->host,
 						      MMC_SEND_TUNING_BLOCK);
 		mmc_host_clk_release(card->host);
+	}
+
+	if (err) {
+		if (card->sw_caps.uhs_max_dtr == UHS_SDR104_MAX_DTR)
+			card->sw_caps.uhs_max_dtr = UHS_SDR50_MAX_DTR;
+		else if (card->sw_caps.uhs_max_dtr == UHS_SDR50_MAX_DTR)
+			card->sw_caps.uhs_max_dtr = UHS_SDR25_MAX_DTR;
+		else if (card->sw_caps.uhs_max_dtr == UHS_SDR25_MAX_DTR)
+			card->sw_caps.uhs_max_dtr = UHS_SDR12_MAX_DTR;
+		else
+			goto out;
+		mmc_set_clock(card->host, card->sw_caps.uhs_max_dtr);
+		goto try;
 	}
 
 out:
