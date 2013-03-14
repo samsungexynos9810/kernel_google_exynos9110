47bac337c71638a58b6ec012fe4e23e240f2f14c
Ayoung Sim <a.sim@samsung.com>
2013-03-14 13:39:38 +0900

    video: fbmod: change return type and add destroy function
    This patch changes the return type to int and
    adds the memory destroy function
    
    Change-Id: Id6ac9ab097be980147532da69aa7eea63275697f
    Signed-off-by: Ayoung Sim <a.sim@samsung.com>

--- include/linux/fb.h
+++ include/linux/fb.h
@@ -719,9 +719,12 @@ extern int fb_parse_edid(unsigned char *edid, struct fb_var_screeninfo *var);
 extern const unsigned char *fb_firmware_edid(struct device *device);
 extern void fb_edid_to_monspecs(unsigned char *edid,
 				struct fb_monspecs *specs);
-extern void fb_edid_add_monspecs(unsigned char *edid,
+extern int fb_edid_add_monspecs(unsigned char *edid,
 				 struct fb_monspecs *specs);
 extern void fb_destroy_modedb(struct fb_videomode *modedb);
+extern void fb_destroy_audiodb(struct fb_audio *audiodb);
+extern void fb_destroy_videodb(struct fb_video *videodb);
+extern void fb_destroy_vsdb(struct fb_vendor *vsdb);
 extern int fb_find_mode_cvt(struct fb_videomode *mode, int margins, int rb);
 extern unsigned char *fb_ddc_read(struct i2c_adapter *adapter);
 
