0e16c392addeea3fa765896f09ae980460c43935
Yulgon Kim <yulgon.kim@samsung.com>
2013-07-29 10:55:39 +0900

    mipi-lli: Support MIPI-LLI
    Adding MIPI-LLI driver support for Samsung's SoCs.
    
    Change-Id: I658d8d483a5ebab7e7ad49ab851aed5053a6bb8e
    Signed-off-by: Yulgon Kim <yulgon.kim@samsung.com>

---
+++ drivers/misc/mipi-lli/exynos-mipi-lli-mphy.c
@@ -0,0 +1,133 @@
+/*
+ * Exynos MIPI-LLI-MPHY driver
+ *
+ * Copyright (C) 2013 Samsung Electronics Co.Ltd
+ * Author: Yulgon Kim <yulgon.kim@samsung.com>
+ *
+ * This program is free software; you can redistribute  it and/or modify it
+ * under  the terms of  the GNU General  Public License as published by the
+ * Free Software Foundation;  either version 2 of the  License, or (at your
+ * option) any later version.
+ */
+
+#include <linux/clk.h>
+#include <linux/delay.h>
+#include <linux/err.h>
+#include <linux/interrupt.h>
+#include <linux/io.h>
+#include <linux/module.h>
+#include <linux/platform_device.h>
+
+#include "exynos-mipi-lli-mphy.h"
+
+struct device *lli_mphy;
+
+int exynos_mphy_init(struct exynos_mphy *phy)
+{
+	writel(0x45, phy->loc_regs + PHY_TX_HS_SYNC_LENGTH(0));
+
+	return 0;
+}
+
+int exynos_mphy_cmn_init(struct exynos_mphy *phy)
+{
+	if (phy->is_shared_clk)
+		writel(0x00, phy->loc_regs + (0x4f*4));
+	else
+		writel(0xF9, phy->loc_regs + (0x4f*4));
+
+	/* PLL power off */
+	writel(0x0, phy->loc_regs + (0x1A*4));
+
+	return 0;
+}
+
+int exynos_mphy_ovtm_init(struct exynos_mphy *phy)
+{
+	if (!phy->is_shared_clk)
+		writel(0, phy->loc_regs + (0x20*4));
+	else
+		writel(1, phy->loc_regs + (0x20*4));
+
+	writel(0x02, phy->loc_regs + (0x16*4));
+	writel(0x8F, phy->loc_regs + (0x45*4));
+	/* For PWM3,4,5G */
+	writel(0x1A, phy->loc_regs + (0x40*4));
+
+	return 0;
+}
+
+static int exynos_mphy_shutdown(struct exynos_mphy *phy)
+{
+	return 0;
+}
+
+static int exynos_mipi_lli_mphy_probe(struct platform_device *pdev)
+{
+	struct exynos_mphy *mphy;
+	struct device *dev = &pdev->dev;
+	struct resource *res;
+	void __iomem *regs;
+	int ret = 0;
+
+	mphy = devm_kzalloc(dev, sizeof(struct exynos_mphy), GFP_KERNEL);
+	if (!mphy) {
+		dev_err(dev, "not enough memory\n");
+		return -ENOMEM;
+	}
+
+	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	if (!res) {
+		dev_err(dev, "cannot find register resource 0\n");
+		return -ENXIO;
+	}
+
+	regs = devm_request_and_ioremap(dev, res);
+	if (!regs) {
+		dev_err(dev, "cannot request_and_map registers\n");
+		return -EADDRNOTAVAIL;
+	}
+
+	spin_lock_init(&mphy->lock);
+	mphy->dev = dev;
+	mphy->loc_regs = regs;
+	mphy->init = exynos_mphy_init;
+	mphy->cmn_init = exynos_mphy_cmn_init;
+	mphy->ovtm_init = exynos_mphy_ovtm_init;
+	mphy->shutdown = exynos_mphy_shutdown;
+
+	mphy->lane = 1;
+	mphy->default_mode = PWM_G1;
+	mphy->is_shared_clk = true;
+
+	platform_set_drvdata(pdev, mphy);
+	lli_mphy = dev;
+
+	return ret;
+}
+
+static int exynos_mipi_lli_mphy_remove(struct platform_device *pdev)
+{
+	return 0;
+}
+
+struct device *exynos_get_mphy(void)
+{
+	return lli_mphy;
+}
+EXPORT_SYMBOL(exynos_get_mphy);
+
+static struct platform_driver exynos_mipi_lli_mphy_driver = {
+	.probe		= exynos_mipi_lli_mphy_probe,
+	.remove		= exynos_mipi_lli_mphy_remove,
+	.driver		= {
+		.name	= "exynos-mipi-lli-mphy",
+		.owner	= THIS_MODULE,
+	},
+};
+
+module_platform_driver(exynos_mipi_lli_mphy_driver);
+
+MODULE_DESCRIPTION("Exynos MIPI-LLI MPHY driver");
+MODULE_AUTHOR("Yulgon Kim <yulgon.kim@samsung.com>");
+MODULE_LICENSE("GPL");
---
+++ include/linux/mipi-lli.h
@@ -0,0 +1,84 @@
+/*
+ * MIPI-LLI driver
+ *
+ * Copyright (C) 2013 Samsung Electronics Co.Ltd
+ * Author: Yulgon Kim <yulgon.kim@samsung.com>
+ *
+ * This program is free software; you can redistribute  it and/or modify it
+ * under  the terms of  the GNU General  Public License as published by the
+ * Free Software Foundation;  either version 2 of the  License, or (at your
+ * option) any later version.
+ */
+
+#ifndef __MIPI_LLI_H
+#define __MIPI_LLI_H
+
+#include <linux/miscdevice.h>
+
+struct mipi_lli_ipc_handler {
+	void *data;
+	void (*handler)(void *, u32);
+};
+
+struct mipi_lli {
+	const struct lli_driver *driver;	/* hw-specific hooks */
+
+	struct miscdevice misc_dev;
+
+	/* Flags that need to be manipulated atomically because they can
+	 * change while the host controller is running.  Always use
+	 * set_bit() or clear_bit() to change their values.
+	 */
+	unsigned long		flags;
+
+	unsigned int		irq;		/* irq allocated */
+	unsigned int		irq_sig;	/* irq_sig allocated */
+	void __iomem		*regs;		/* device memory/io */
+	void __iomem		*remote_regs;	/* device memory/io */
+	void __iomem		*sys_regs;	/* device memory/io */
+
+	struct mutex		*lli_mutex;
+	struct spin_lock	*lock;
+	struct device		*dev;
+	struct device		*mphy;
+
+	void __iomem		*shdmem_addr;
+	u32			shdmem_size;
+	dma_addr_t		phy_addr;
+	int			state;
+	bool			is_master;
+
+	struct mipi_lli_ipc_handler hd;
+};
+
+struct lli_driver {
+	int	flags;
+
+	int	(*init)(struct mipi_lli *lli);
+	int	(*exit)(struct mipi_lli *lli);
+	int	(*set_master)(struct mipi_lli *lli, bool is_master);
+	int	(*link_startup_mount)(struct mipi_lli *lli);
+	int	(*get_status)(struct mipi_lli *lli);
+
+	int	(*send_signal)(struct mipi_lli *lli, u32 cmd);
+	int	(*reset_signal)(struct mipi_lli *lli);
+	int	(*read_signal)(struct mipi_lli *lli);
+};
+
+extern int mipi_lli_add_driver(struct device *dev,
+			       const struct lli_driver *lli_driver,
+			       int irq);
+extern void mipi_lli_remove_driver(struct mipi_lli *lli);
+
+extern void __iomem *mipi_lli_request_sh_region(u32 sh_addr, u32 size);
+extern void mipi_lli_release_sh_region(void *rgn);
+extern unsigned long mipi_lli_get_phys_base(void);
+extern unsigned long mipi_lli_get_phys_size(void);
+extern int mipi_lli_register_handler(void (*handler)(void *, u32), void *data);
+extern int mipi_lli_unregister_handler(void (*handler)(void *, u32));
+extern void mipi_lli_send_interrupt(u32 cmd);
+extern void mipi_lli_reset_interrupt(void);
+extern u32 mipi_lli_read_interrupt(void);
+extern void mipi_lli_reload(void);
+
+#endif /* __MIPI_LLI_H */
