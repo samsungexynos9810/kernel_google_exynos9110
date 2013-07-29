fb0f31ab9cc1096a04f4ba22b65f6048242f035a
lakkyung.jung <lakkyung.jung@samsung.com>
2013-07-29 14:38:22 +0900

    ARM: EXYNOS: Revert machine file of exynos5-dt.
    Change-Id: Ib6d49bbbfad94d7248762de36a29853f9ef1c25e
    Signed-off-by: lakkyung.jung <lakkyung.jung@samsung.com>

--- arch/arm/mach-exynos/mach-exynos5-dt.c
+++ arch/arm/mach-exynos/mach-exynos5-dt.c
@@ -57,7 +57,6 @@ static void __init exynos5_dt_machine_init(void)
 
 static char const *exynos5_dt_compat[] __initdata = {
 	"samsung,exynos5250",
-	"samsung,exynos5430",
 	"samsung,exynos5440",
 	NULL
 };
