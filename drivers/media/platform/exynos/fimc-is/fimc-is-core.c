7d10097df5780d8be7156a151f51ea082980d3e5
Wooki Min <wooki.min@samsung.com>
2013-08-14 10:54:12 +0900

    media: fimc-is: support platform data for fimc-is
    Change-Id: I138b019f456d87316c37adb8955efc4dadb11e13
    Signed-off-by: Wooki Min <wooki.min@samsung.com>

--- drivers/media/platform/exynos/fimc-is/fimc-is-core.c
+++ drivers/media/platform/exynos/fimc-is/fimc-is-core.c
@@ -36,6 +36,7 @@
 #include <linux/exynos_iovmm.h>
 #ifdef CONFIG_OF
 #include <linux/of.h>
+#include <linux/of_gpio.h>
 #endif
 
 #include "fimc-is-core.h"
@@ -1008,14 +1009,162 @@ static int fimc_is_fault_handler(struct device *dev, const char *mmuname,
 }
 #endif
 
+#ifdef CONFIG_OF
+static struct exynos5_platform_fimc_is *fimc_is_parse_dt(struct device *dev)
+{
+	struct exynos5_platform_fimc_is *pdata;
+	struct device_node *np = dev->of_node;
+	struct fimc_is_gpio_info *gpio_info;
+
+	if (!np)
+		return ERR_PTR(-ENOENT);
+
+	pdata = kzalloc(sizeof(struct exynos5_platform_fimc_is), GFP_KERNEL);
+	if (!pdata) {
+		printk(KERN_ERR "%s: no memory for platform data\n", __func__);
+		return ERR_PTR(-ENOMEM);
+	}
+
+	pdata->cfg_gpio = exynos5_fimc_is_cfg_gpio;
+	pdata->clk_cfg = exynos5430_fimc_is_cfg_clk;
+	pdata->clk_on = exynos5430_fimc_is_clk_on;
+	pdata->clk_off = exynos5430_fimc_is_clk_off;
+	pdata->sensor_clock_on = exynos5430_fimc_is_sensor_clk_on;
+	pdata->sensor_clock_off = exynos5430_fimc_is_sensor_clk_off;
+	pdata->sensor_power_on = exynos5_fimc_is_sensor_power_on;
+	pdata->sensor_power_off = exynos5_fimc_is_sensor_power_off;
+	pdata->print_cfg = exynos5_fimc_is_print_cfg;
+
+	gpio_info = kzalloc(sizeof(struct fimc_is_gpio_info), GFP_KERNEL);
+	if (!gpio_info) {
+		printk(KERN_ERR "%s: no memory for gpio_info\n", __func__);
+		return ERR_PTR(-ENOMEM);
+	}
+
+	gpio_info->gpio_main_rst = of_get_gpio(np, 0);
+	if (!gpio_is_valid(gpio_info->gpio_main_rst)) {
+		dev_err(dev, "failed to get main reset gpio\n");
+		return ERR_PTR(-EINVAL);
+	}
+
+	gpio_info->gpio_main_sda = of_get_gpio(np, 1);
+	if (!gpio_is_valid(gpio_info->gpio_main_sda)) {
+		dev_err(dev, "failed to get main sda gpio\n");
+		return ERR_PTR(-EINVAL);
+	}
+
+	gpio_info->gpio_main_scl = of_get_gpio(np, 2);
+	if (!gpio_is_valid(gpio_info->gpio_main_scl)) {
+		dev_err(dev, "failed to get main scl gpio\n");
+		return ERR_PTR(-EINVAL);
+	}
+
+	gpio_info->gpio_main_mclk = of_get_gpio(np, 3);
+	if (!gpio_is_valid(gpio_info->gpio_main_mclk)) {
+		dev_err(dev, "failed to get main mclk gpio\n");
+		return ERR_PTR(-EINVAL);
+	}
+
+	gpio_info->gpio_main_flash_en = of_get_gpio(np, 4);
+	if (!gpio_is_valid(gpio_info->gpio_main_flash_en)) {
+		dev_err(dev, "failed to get main flash_en gpio\n");
+		return ERR_PTR(-EINVAL);
+	}
+
+	gpio_info->gpio_main_flash_torch = of_get_gpio(np, 5);
+	if (!gpio_is_valid(gpio_info->gpio_main_flash_torch)) {
+		dev_err(dev, "failed to get main flash_torch gpio\n");
+		return ERR_PTR(-EINVAL);
+	}
+
+	gpio_info->gpio_vt_rst = of_get_gpio(np, 6);
+	if (!gpio_is_valid(gpio_info->gpio_vt_rst)) {
+		dev_err(dev, "failed to get vt reset gpio\n");
+		return ERR_PTR(-EINVAL);
+	}
+
+	gpio_info->gpio_vt_sda = of_get_gpio(np, 7);
+	if (!gpio_is_valid(gpio_info->gpio_vt_sda)) {
+		dev_err(dev, "failed to get vt sda gpio\n");
+		return ERR_PTR(-EINVAL);
+	}
+
+	gpio_info->gpio_vt_scl = of_get_gpio(np, 8);
+	if (!gpio_is_valid(gpio_info->gpio_vt_scl)) {
+		dev_err(dev, "failed to get vt scl gpio\n");
+		return ERR_PTR(-EINVAL);
+	}
+
+	gpio_info->gpio_vt_mclk = of_get_gpio(np, 9);
+	if (!gpio_is_valid(gpio_info->gpio_vt_mclk)) {
+		dev_err(dev, "failed to get vt mclk gpio\n");
+		return ERR_PTR(-EINVAL);
+	}
+
+	gpio_info->gpio_spi_clk = of_get_gpio(np, 10);
+	if (!gpio_is_valid(gpio_info->gpio_spi_clk)) {
+		dev_err(dev, "failed to get spi clk gpio\n");
+		return ERR_PTR(-EINVAL);
+	}
+
+	gpio_info->gpio_spi_csn = of_get_gpio(np, 11);
+	if (!gpio_is_valid(gpio_info->gpio_spi_csn)) {
+		dev_err(dev, "failed to get spi csn gpio\n");
+		return ERR_PTR(-EINVAL);
+	}
+
+	gpio_info->gpio_spi_miso = of_get_gpio(np, 12);
+	if (!gpio_is_valid(gpio_info->gpio_spi_miso)) {
+		dev_err(dev, "failed to get spi miso gpio\n");
+		return ERR_PTR(-EINVAL);
+	}
+
+	gpio_info->gpio_spi_mosi = of_get_gpio(np, 13);
+	if (!gpio_is_valid(gpio_info->gpio_spi_mosi)) {
+		dev_err(dev, "failed to get spi mosi gpio\n");
+		return ERR_PTR(-EINVAL);
+	}
+
+	gpio_info->gpio_uart_txd = of_get_gpio(np, 14);
+	if (!gpio_is_valid(gpio_info->gpio_uart_txd)) {
+		dev_err(dev, "failed to get uart txd gpio\n");
+		return ERR_PTR(-EINVAL);
+	}
+
+	gpio_info->gpio_uart_rxd = of_get_gpio(np, 15);
+	if (!gpio_is_valid(gpio_info->gpio_uart_rxd)) {
+		dev_err(dev, "failed to get uart rxd gpio\n");
+		return ERR_PTR(-EINVAL);
+	}
+
+	pdata->_gpio_info = gpio_info;
+	dev->platform_data = pdata;
+
+	return pdata;
+}
+#else
+static struct exynos5_platform_fimc_is *fimc_is_parse_dt(struct device *dev)
+{
+	return ERR_PTR(-EINVAL);
+}
+#endif
+
 static int fimc_is_probe(struct platform_device *pdev)
 {
+	struct exynos5_platform_fimc_is *pdata;
 	struct resource *mem_res;
 	struct resource *regs_res;
 	struct fimc_is_core *core;
 	int ret = -ENODEV;
 
 	pr_info("%s\n", __func__);
+
+	pdata = dev_get_platdata(&pdev->dev);
+	if (!pdata) {
+		pdata = fimc_is_parse_dt(&pdev->dev);
+		if (IS_ERR(pdata))
+			return PTR_ERR(pdata);
+	}
 
 	core = kzalloc(sizeof(struct fimc_is_core), GFP_KERNEL);
 	if (!core)
@@ -1024,7 +1173,7 @@ static int fimc_is_probe(struct platform_device *pdev)
 	is_dev = &pdev->dev;
 
 	core->pdev = pdev;
-	core->pdata = pdev->dev.platform_data;
+	core->pdata = pdata;
 	core->id = pdev->id;
 	core->debug_cnt = 0;
 	device_init_wakeup(&pdev->dev, true);
