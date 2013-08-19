fbd0641b1c2644c42ac34789ad17e83757dc9da3
Hyunwoong Kim <khw0178.kim@samsung.com>
2013-08-20 08:31:03 +0900

    ARM: MM: fix error log from atomic_pool_init
    If CMA is enabled, kernel tries to create atomic pool via CMA.
    However, there is no contiguous region for CMA. It causes kernel
    to print error log. This patch fix error log by switching the
    function creating atomic pool.
    
    Change-Id: I4e84ae88fd377f9e5ad963cb75293220db11f1ee
    Signed-off-by: Hyunwoong Kim <khw0178.kim@samsung.com>

--- arch/arm/mm/dma-mapping.c
+++ arch/arm/mm/dma-mapping.c
@@ -360,6 +360,7 @@ void __init init_dma_coherent_pool_size(unsigned long size)
 /*
  * Initialise the coherent pool for atomic allocations.
  */
+__attribute__ ((unused))
 static int __init atomic_pool_init(void)
 {
 	struct dma_pool *pool = &atomic_pool;
@@ -410,10 +411,58 @@ no_bitmap:
 	       (unsigned)pool->size / 1024);
 	return -ENOMEM;
 }
+
+static int __init exynos_atomic_pool_init(void)
+{
+	struct dma_pool *pool = &atomic_pool;
+	pgprot_t prot = pgprot_dmacoherent(pgprot_kernel);
+	gfp_t gfp = GFP_KERNEL | GFP_DMA;
+	unsigned long nr_pages = pool->size >> PAGE_SHIFT;
+	unsigned long *bitmap;
+	struct page *page;
+	struct page **pages;
+	void *ptr;
+	int bitmap_size = BITS_TO_LONGS(nr_pages) * sizeof(long);
+
+	bitmap = kzalloc(bitmap_size, GFP_KERNEL);
+	if (!bitmap)
+		goto no_bitmap;
+
+	pages = kzalloc(nr_pages * sizeof(struct page *), GFP_KERNEL);
+	if (!pages)
+		goto no_pages;
+
+	ptr = __alloc_remap_buffer(NULL, pool->size, gfp, prot, &page,
+					   exynos_atomic_pool_init);
+	if (ptr) {
+		int i;
+
+		for (i = 0; i < nr_pages; i++)
+			pages[i] = page + i;
+
+		spin_lock_init(&pool->lock);
+		pool->vaddr = ptr;
+		pool->pages = pages;
+		pool->bitmap = bitmap;
+		pool->nr_pages = nr_pages;
+		pr_info("DMA: preallocated %u KiB pool for atomic coherent allocations\n",
+		       (unsigned)pool->size / 1024);
+		return 0;
+	}
+
+	kfree(pages);
+no_pages:
+	kfree(bitmap);
+no_bitmap:
+	pr_err("DMA: failed to allocate %u KiB pool for atomic coherent allocation\n",
+	       (unsigned)pool->size / 1024);
+	return -ENOMEM;
+}
 /*
  * CMA is activated by core_initcall, so we must be called after it.
  */
-postcore_initcall(atomic_pool_init);
+
+postcore_initcall(exynos_atomic_pool_init);
 
 struct dma_contig_early_reserve {
 	phys_addr_t base;
