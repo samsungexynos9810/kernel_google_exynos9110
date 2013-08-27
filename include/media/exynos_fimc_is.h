a410b1813a966b817e4f8ce8a7f948589b4280ec
Teahyung Kim <tkon.kim@samsung.com>
2013-08-27 10:59:08 +0900

    media: fimc-is: added dvfs macro, enum to header
    For refactoring DVFS, some macro, enum and var are added to header.
    They can be used in fimc-is driver and device.
    
    Change-Id: Icd57d3b68802a5ec0f2e2516630763e036146e2c
    Signed-off-by: Teahyung Kim <tkon.kim@samsung.com>

--- include/media/exynos_fimc_is.h
+++ include/media/exynos_fimc_is.h
@@ -11,6 +11,9 @@
 #ifndef EXYNOS_FIMC_IS_H_
 #define EXYNOS_FIMC_IS_H_ __FILE__
 
+#define FIMC_IS_MAKE_QOS_IDX_NM(__LV) __LV ## _IDX
+#define FIMC_IS_DECLARE_QOS_ENUM(__TYPE) enum FIMC_IS_DVFS_ ## __TYPE ## _LV_IDX
+
 #include <linux/videodev2.h>
 #if defined(CONFIG_ARCH_EXYNOS4)
 #include <media/s5p_fimc.h>
@@ -29,6 +32,53 @@
 #define UART_ISP_RATIO			1
 
 #define to_fimc_is_plat(d)	(to_platform_device(d)->dev.platform_data)
+
+/* FIMC-IS DVFS LEVEL enum (INT, MIF, I2C) */
+enum FIMC_IS_INT_LV {
+	FIMC_IS_INT_L0,
+	FIMC_IS_INT_L1,
+	FIMC_IS_INT_L1_1,
+	FIMC_IS_INT_L1_2,
+	FIMC_IS_INT_L1_3,
+};
+
+enum FIMC_IS_MIF_LV {
+	FIMC_IS_MIF_L0,
+	FIMC_IS_MIF_L1,
+	FIMC_IS_MIF_L2,
+	FIMC_IS_MIF_L3,
+	FIMC_IS_MIF_L4,
+};
+
+/*
+ * On some soc, It needs to notify change of INT clock to F/W.
+ * Because I2C clock can be take affect from other clock change(like INT)
+ */
+enum FIMC_IS_I2C_LV {
+	FIMC_IS_I2C_L0,
+	FIMC_IS_I2C_L1,
+	FIMC_IS_I2C_L1_1,
+	FIMC_IS_I2C_L1_3,
+};
+
+/* FIMC-IS DVFS SCENARIO enum */
+enum FIMC_IS_SCENARIO_ID {
+	FIMC_IS_SN_DEFAULT,
+	FIMC_IS_SN_FRONT_PREVIEW,
+	FIMC_IS_SN_FRONT_CAPTURE,
+	FIMC_IS_SN_FRONT_CAMCORDING,
+	FIMC_IS_SN_FRONT_VT1,
+	FIMC_IS_SN_FRONT_VT2,
+	FIMC_IS_SN_REAR_PREVIEW,
+	FIMC_IS_SN_REAR_CAPTURE,
+	FIMC_IS_SN_REAR_CAMCORDING,
+	FIMC_IS_SN_DUAL_PREVIEW,
+	FIMC_IS_SN_DUAL_CAPTURE,
+	FIMC_IS_SN_DUAL_CAMCORDING,
+	FIMC_IS_SN_HIGH_SPEED_FPS,
+	FIMC_IS_SN_DIS_ENABLE,
+	FIMC_IS_SN_MAX,
+};
 
 #if defined(CONFIG_ARCH_EXYNOS4)
 #define FIMC_IS_MAX_DIV_CLOCKS		2
@@ -349,6 +399,14 @@ struct exynos5_platform_fimc_is {
 	int	(*sensor_power_on)(struct platform_device *pdev, int sensor_id);
 	int	(*sensor_power_off)(struct platform_device *pdev, int sensor_id);
 	int	(*print_cfg)(struct platform_device *pdev, u32 channel);
+
+	/* These fields are to return qos value for dvfs scenario */
+	u32	*int_qos_table;
+	u32	*mif_qos_table;
+	u32	*i2c_qos_table;
+	int	(*get_int_qos)(int scenario_id);
+	int	(*get_mif_qos)(int scenario_id);
+	int	(*get_i2c_qos)(int scenario_id);
 };
 
 extern void exynos5_fimc_is_set_platdata(struct exynos5_platform_fimc_is *pd);
