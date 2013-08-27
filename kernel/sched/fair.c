e8dc4a01cbffbc274aab254070d24753cfd924b6
Chris Redpath <chris.redpath@arm.com>
2013-08-27 21:24:30 +0900

    HMP: Modify the runqueue stats to add a new child stat
    Originally:
    
    Add a new unweighted load runqueue runnable ratio, made from
    the sum of all runnable tasks on the runqueue. The sum is updated
    when a task is updated, and tasks are added/removed as they
    are enqueued and dequeued.
    
    The resolution of the comparison is reduced so that there are
    around 10 bands of busy-ness for each runqueue. The intent is
    to match busy-ness more often and choose the least-recently-
    disturbed runqueue.
    
    HOWEVER:
    
    This patch is accidentally broken and results in the following
    algorithm in hmp_domain_min_load:
    
    * If any CPU is zero, the overall load is zero
    * If no CPUs are idle, the domain is 'fully loaded'
    
    Additionally, the time since last migration count is used to
    discriminate between idle CPUs. This is a beneficial scheduling
    strategy in at least the Quadrant CPU Benchmark.
    
    No other benchmarks have been tested and since this is strictly
    speaking a broken patch, their results might be odd.
    
    Change-Id: I7c9a6f39b42d79f667f497510362bf0803efa36a
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>
    Signed-off-by: Hwang Seungkon <skon.hwang@samsung.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1582,9 +1582,10 @@ static inline void __update_task_entity_contrib(struct sched_entity *se)
 }
 
 /* Compute the current contribution to load_avg by se, return any delta */
-static long __update_entity_load_avg_contrib(struct sched_entity *se)
+static long __update_entity_load_avg_contrib(struct sched_entity *se, long *ratio)
 {
 	long old_contrib = se->avg.load_avg_contrib;
+	long old_ratio   = se->avg.load_avg_ratio;
 
 	if (entity_is_task(se)) {
 		__update_task_entity_contrib(se);
@@ -1593,6 +1594,8 @@ static long __update_entity_load_avg_contrib(struct sched_entity *se)
 		__update_group_entity_contrib(se);
 	}
 
+	if (ratio)
+		*ratio = se->avg.load_avg_ratio - old_ratio;
 	return se->avg.load_avg_contrib - old_contrib;
 }
 
@@ -1612,7 +1615,7 @@ static inline void update_entity_load_avg(struct sched_entity *se,
 					  int update_cfs_rq)
 {
 	struct cfs_rq *cfs_rq = cfs_rq_of(se);
-	long contrib_delta;
+	long contrib_delta, ratio_delta;
 	u64 now;
 	int cpu = -1;   /* not used in normal case */
 
@@ -1632,15 +1635,17 @@ static inline void update_entity_load_avg(struct sched_entity *se,
 			cfs_rq->curr == se, cpu))
 		return;
 
-	contrib_delta = __update_entity_load_avg_contrib(se);
+	contrib_delta = __update_entity_load_avg_contrib(se, &ratio_delta);
 
 	if (!update_cfs_rq)
 		return;
 
-	if (se->on_rq)
+	if (se->on_rq) {
 		cfs_rq->runnable_load_avg += contrib_delta;
-	else
+		rq_of(cfs_rq)->avg.child_avg_ratio += ratio_delta;
+	} else {
 		subtract_blocked_load_contrib(cfs_rq, -contrib_delta);
+	}
 }
 
 /*
@@ -1727,6 +1732,8 @@ static inline void enqueue_entity_load_avg(struct cfs_rq *cfs_rq,
 	}
 
 	cfs_rq->runnable_load_avg += se->avg.load_avg_contrib;
+	rq_of(cfs_rq)->avg.child_avg_ratio += se->avg.load_avg_ratio;
+
 	/* we force update consideration on load-balancer moves */
 	update_cfs_rq_blocked_load(cfs_rq, !wakeup);
 }
@@ -1745,6 +1752,8 @@ static inline void dequeue_entity_load_avg(struct cfs_rq *cfs_rq,
 	update_cfs_rq_blocked_load(cfs_rq, !sleep);
 
 	cfs_rq->runnable_load_avg -= se->avg.load_avg_contrib;
+	rq_of(cfs_rq)->avg.child_avg_ratio -= se->avg.load_avg_ratio;
+
 	if (sleep) {
 		cfs_rq->blocked_load_avg += se->avg.load_avg_contrib;
 		se->avg.decay_count = atomic64_read(&cfs_rq->decay_counter);
@@ -3966,8 +3975,8 @@ static inline unsigned int hmp_domain_min_load(struct hmp_domain *hmpd,
 			avg->hmp_last_up_migration : avg->hmp_last_down_migration;
 
 		/* don't use the divisor in the loop, just at the end */
-		contrib = avg->runnable_avg_sum * scale_load_down(1024);
-		scaled_contrib = contrib >> 22;
+		contrib = avg->child_avg_ratio * scale_load_down(1024);
+		scaled_contrib = contrib >> 13;
 
 		if ((contrib < min_runnable_load) ||
 				(scaled_contrib == scaled_min_runnable_load &&
@@ -3990,8 +3999,8 @@ static inline unsigned int hmp_domain_min_load(struct hmp_domain *hmpd,
 		*min_cpu = min_cpu_runnable_temp;
 
 	/* domain will often have at least one empty CPU */
-	return min_runnable_load ? min_runnable_load / (LOAD_AVG_MAX + 1) : 0;
-
+	trace_printk("hmp_domain_min_load returning %lu\n", min_runnable_load > 1023 ? 1023 : min_runnable_load);
+	return min_runnable_load > 1023 ? 1023 : min_runnable_load; /* ? min_runnable_load / (LOAD_AVG_MAX + 1) : 0*/
 }
 
 /*
