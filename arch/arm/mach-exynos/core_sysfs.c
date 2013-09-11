769af331986064432a94f659d1c0bb3984102fea
Hwang Seungkon <skon.hwang@samsung.com>
2013-09-11 21:10:17 +0900

    ARM: EXYNOS: Change the new API for core state.
    Change-Id: If3d3aab75b230bea7311a3294abe528327771f24
    Signed-off-by: Hwang Seungkon <skon.hwang@samsung.com>

--- arch/arm/mach-exynos/core_sysfs.c
+++ arch/arm/mach-exynos/core_sysfs.c
@@ -17,11 +17,14 @@
 #include <linux/sysfs.h>
 #include <linux/moduleparam.h>
 #include <mach/regs-pmu.h>
+#include "common.h"
 
 static struct bus_type core_subsys = {
 	.name = "b.L",
 	.dev_name = "b.L",
 };
+
+unsigned int (*cpu_state)(unsigned int cpu_id);
 
 static ssize_t exynos5_core_status_show(struct kobject *kobj,
 			struct kobj_attribute *attr, char *buf)
@@ -30,8 +33,9 @@ static ssize_t exynos5_core_status_show(struct kobject *kobj,
 	int cpu;
 
 	for_each_possible_cpu(cpu) {
-		unsigned int v = __raw_readl(EXYNOS_ARM_CORE_STATUS(cpu)) & 3;
-		n += scnprintf(buf + n, 11, "cpu %d : %d\n", cpu, v ? 1 : 0);
+		unsigned int v = cpu_state(cpu);
+		n += scnprintf(buf + n, 11, "cpu %d : %d\n", cpu,
+				v == EXYNOS_CORE_PWR_EN ? 1 : 0);
 	}
 
 	return n;
@@ -61,6 +65,9 @@ static int __init exynos5_core_sysfs_init(void)
 	ret = subsys_system_register(&core_subsys, exynos5_core_sysfs_groups);
 	if (ret)
 		pr_err("Fail to register exynos5 core subsys\n");
+
+	if (of_machine_is_compatible("samsung,exynos5430"))
+		cpu_state = exynos5430_cpu_state;
 
 	return ret;
 }
