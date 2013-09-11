2fe69258088d93c6d621814c59a5d5b459561934
Anton Tikhomirov <av.tikhomirov@samsung.com>
2013-09-11 17:13:14 +0900

    USB: dwc3: disable USB2 & USB3 PHYs suspension
    USB2 PHY provide some signals and clock for USB3 PHY. If USB2
    PHY is allowed to suspend, then USB3 PHY cannot operate properly
    if USB2 PHY is suspended (e.g. there is issue when in "Host only"
    mode USB3.0 device can't connect at SuperSpeed). This patch
    disables PHYs suspension to provide SuperSpeed connectivity.
    
    Change-Id: Ida6d8f678a8fe0c8cb2cc30370f6666c7174d145
    Signed-off-by: Anton Tikhomirov <av.tikhomirov@samsung.com>

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -119,6 +119,14 @@ static void dwc3_core_soft_reset(struct dwc3 *dwc)
 	reg = dwc3_readl(dwc->regs, DWC3_GCTL);
 	reg &= ~DWC3_GCTL_CORESOFTRESET;
 	dwc3_writel(dwc->regs, DWC3_GCTL, reg);
+
+	reg = dwc3_readl(dwc->regs, DWC3_GUSB2PHYCFG(0));
+	reg &= ~DWC3_GUSB2PHYCFG_SUSPHY;
+	dwc3_writel(dwc->regs, DWC3_GUSB2PHYCFG(0), reg);
+
+	reg = dwc3_readl(dwc->regs, DWC3_GUSB3PIPECTL(0));
+	reg &= ~DWC3_GUSB3PIPECTL_SUSPHY;
+	dwc3_writel(dwc->regs, DWC3_GUSB3PIPECTL(0), reg);
 }
 
 /**
