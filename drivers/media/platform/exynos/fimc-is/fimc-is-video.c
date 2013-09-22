e44495edbb9ae8208e7d0fea37055faaf975a9ac
Teahyung Kim <tkon.kim@samsung.com>
2013-09-22 14:03:33 +0900

    media: fimc-is: incresed meta size from 16k to 20k
    * This patch was from Exynos5260 for fimc-is unification.
    
    Change-Id: I1c5add7487672bb85cb3f8dd19f7aec4d78b099d
    Signed-off-by: Teahyung Kim <tkon.kim@samsung.com>

--- drivers/media/platform/exynos/fimc-is/fimc-is-video.c
+++ drivers/media/platform/exynos/fimc-is/fimc-is-video.c
@@ -46,7 +46,11 @@
 #include "fimc-is-err.h"
 
 #define SPARE_PLANE 1
+#if defined(CONFIG_SOC_EXYNOS3470) || defined(CONFIG_SOC_EXYNOS5260)
+#define SPARE_SIZE (20 * 1024)
+#else
 #define SPARE_SIZE (16 * 1024)
+#endif
 
 struct fimc_is_fmt fimc_is_formats[] = {
 	 {
