4149945ea06eea0c215570a38f161dfacbc31206
eunki.kim <eunki_kim@samsung.com>
2013-10-02 23:01:51 +0900

    pinctrl: samsung: Fix pm function
    gpz's power is controlled by audio block. This patch does not execute
    pm function when its ctrl has no suspend or resume function.
    
    Change-Id: Icbddd23ef3a312b4c58dec707e445ef213340ef9
    Signed-off-by: eunki.kim <eunki_kim@samsung.com>

--- drivers/pinctrl/pinctrl-samsung.c
+++ drivers/pinctrl/pinctrl-samsung.c
@@ -1175,6 +1175,9 @@ static void samsung_pinctrl_suspend_dev(
 	void __iomem *virt_base = drvdata->virt_base;
 	int i;
 
+	if (!ctrl->suspend)
+		return;
+
 	for (i = 0; i < ctrl->nr_banks; i++) {
 		struct samsung_pin_bank *bank = &ctrl->pin_banks[i];
 		void __iomem *reg = virt_base + bank->pctl_offset;
@@ -1209,8 +1212,7 @@ static void samsung_pinctrl_suspend_dev(
 
 	pinctrl_force_sleep(pctl_dev);
 
-	if (ctrl->suspend)
-		ctrl->suspend(drvdata);
+	ctrl->suspend(drvdata);
 }
 
 /**
@@ -1228,8 +1230,10 @@ static void samsung_pinctrl_resume_dev(struct samsung_pinctrl_drv_data *drvdata)
 	void __iomem *virt_base = drvdata->virt_base;
 	int i;
 
-	if (ctrl->resume)
-		ctrl->resume(drvdata);
+	if (!ctrl->resume)
+		return;
+
+	ctrl->resume(drvdata);
 
 	pinctrl_force_default(pctl_dev);
 
