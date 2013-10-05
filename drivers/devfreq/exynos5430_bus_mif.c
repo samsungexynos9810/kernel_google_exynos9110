f96f8255b5529c28927fa540beb909f1e92e9013
Sangkyu Kim <skwith.kim@samsung.com>
2013-10-05 20:23:27 +0900

    PM: DEVFREQ: modify lowest level of MIF to 160Mhz
    Change-Id: I1cbec791959606b069065aa85615c7b4b46d2471
    Signed-off-by: Sangkyu Kim <skwith.kim@samsung.com>

--- drivers/devfreq/exynos5430_bus_mif.c
+++ drivers/devfreq/exynos5430_bus_mif.c
@@ -353,7 +353,7 @@ static struct devfreq_simple_ondemand_data exynos5_devfreq_mif_governor_data = {
 };
 
 static struct exynos_devfreq_platdata exynos5430_qos_mif = {
-	.default_qos		= 100000,
+	.default_qos		= 160000,
 };
 
 static struct ppmu_info ppmu_mif[] = {
