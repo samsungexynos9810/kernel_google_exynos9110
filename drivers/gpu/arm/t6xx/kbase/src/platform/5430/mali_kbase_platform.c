48b364152b94d0b83f3891691b96f4d40a46bccc
Kim Seongki <sk1217.kim@samsung.com>
2013-10-07 09:42:54 +0900

    gpu: t6xx: fix the compilation errors when turning on the CONFIG_MALI_T6XX_DEBUG_SYS
    Change-Id: Ib18fa8a82c9e74ec2ca504ccff74b1b6c09aaa72
    Signed-off-by: Kim Seongki <sk1217.kim@samsung.com>

--- drivers/gpu/arm/t6xx/kbase/src/platform/5430/mali_kbase_platform.c
+++ drivers/gpu/arm/t6xx/kbase/src/platform/5430/mali_kbase_platform.c
@@ -340,6 +340,7 @@ static ssize_t set_clock(struct device *dev, struct device_attribute *attr, cons
 	if (!kbdev)
 		return -ENODEV;
 
+#ifdef CONFIG_MALI_T6XX_DVFS
 	platform = (struct exynos_context *) kbdev->platform_context;
 	if (!platform)
 		return -ENODEV;
@@ -371,6 +372,9 @@ static ssize_t set_clock(struct device *dev, struct device_attribute *attr, cons
 	do {
 		tmp = __raw_readl(EXYNOS5430_DIV_STAT_G3D);
 	} while (tmp & 0x1);
+#else /* CONFIG_MALI_T6XX_DVFS */
+	printk("G3D DVFS is disabled. You can not set\n");
+#endif
 
 	return count;
 }
@@ -919,46 +923,6 @@ static ssize_t set_asv(struct device *dev, struct device_attribute *attr, const 
 	return count;
 }
 
-static ssize_t show_affinity_mask(struct device *dev, struct device_attribute *attr, char *buf)
-{
-	struct kbase_device *kbdev;
-	ssize_t ret = 0;
-
-	kbdev = dev_get_drvdata(dev);
-
-	if (!kbdev)
-		return -ENODEV;
-
-	ret += snprintf(buf+ret, PAGE_SIZE-ret, "0x%x", (u32)kbase_js_get_affinity_mask());
-
-	if (ret < PAGE_SIZE - 1) {
-		ret += snprintf(buf+ret, PAGE_SIZE-ret, "\n");
-	} else {
-		buf[PAGE_SIZE-2] = '\n';
-		buf[PAGE_SIZE-1] = '\0';
-		ret = PAGE_SIZE-1;
-	}
-
-	return ret;
-}
-
-static ssize_t set_affinity_mask(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
-{
-	unsigned long mask;
-	struct kbase_device *kbdev;
-	kbdev = dev_get_drvdata(dev);
-
-	if (!kbdev)
-		return -ENODEV;
-
-	if (kstrtoul(buf, 0, &mask) == 0)
-		kbase_js_set_affinity_mask((u32)mask);
-	else
-		pr_info("Invalid mask!!!\n");
-
-	return count;
-}
-
 static ssize_t show_tmu(struct device *dev, struct device_attribute *attr, char *buf)
 {
 	ssize_t ret = 0;
@@ -981,6 +945,7 @@ static ssize_t show_tmu(struct device *dev, struct device_attribute *attr, char 
 
 static ssize_t set_tmu_control(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
 {
+#ifdef CONFIG_MALI_T6XX_DVFS
 	if (sysfs_streq("0", buf)) {
 		if (gpu_voltage_margin != 0)
 			kbase_set_power_margin(0);
@@ -991,6 +956,9 @@ static ssize_t set_tmu_control(struct device *dev, struct device_attribute *attr
 		tmu_on_off = true;
 	else
 		printk("invalid val -only [0 or 1] is accepted\n");
+#else /* CONFIG_MALI_T6XX_DVFS */
+	printk("G3D DVFS is disabled. You can not set\n");
+#endif
 
 	return count;
 }
@@ -1031,7 +999,6 @@ DEVICE_ATTR(time_in_state, S_IRUGO|S_IWUSR, show_time_in_state, set_time_in_stat
 DEVICE_ATTR(tmu, S_IRUGO|S_IWUSR, show_tmu, set_tmu_control);
 DEVICE_ATTR(utilization, S_IRUGO|S_IWUSR, show_utilization, NULL);
 DEVICE_ATTR(dvfs_table, S_IRUGO|S_IWUSR, show_dvfs_table, NULL);
-DEVICE_ATTR(affinity_mask, S_IRUGO|S_IWUSR, show_affinity_mask, set_affinity_mask);
 DEVICE_ATTR(power_state, S_IRUGO|S_IWUSR, show_power_state, NULL);
 
 int kbase_platform_create_sysfs_file(struct device *dev)
@@ -1096,11 +1063,6 @@ int kbase_platform_create_sysfs_file(struct device *dev)
 		goto out;
 	}
 
-	if (device_create_file(dev, &dev_attr_affinity_mask)) {
-		dev_err(dev, "Couldn't create sysfs file [affinity_mask]\n");
-		goto out;
-	}
-
 	if (device_create_file(dev, &dev_attr_power_state)) {
 		dev_err(dev, "Couldn't create sysfs file [power_state]\n");
 		goto out;
@@ -1125,7 +1087,6 @@ void kbase_platform_remove_sysfs_file(struct device *dev)
 	device_remove_file(dev, &dev_attr_tmu);
 	device_remove_file(dev, &dev_attr_utilization);
 	device_remove_file(dev, &dev_attr_dvfs_table);
-	device_remove_file(dev, &dev_attr_affinity_mask);
 	device_remove_file(dev, &dev_attr_power_state);
 }
 #endif /* CONFIG_MALI_T6XX_DEBUG_SYS */
