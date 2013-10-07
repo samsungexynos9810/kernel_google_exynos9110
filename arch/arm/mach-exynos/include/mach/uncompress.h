1fb29bba86bfa9feaa22425473c8c971cc13df3c
lakkyung.jung <lakkyung.jung@samsung.com>
2013-10-07 22:28:46 +0900

    ARM: EXYNOS: Add to support uncompressed message for exynos5422
    Change-Id: I7894982b8ba7fd29fbca4c6faf45738f93992f41
    Signed-off-by: lakkyung.jung <lakkyung.jung@samsung.com>

--- arch/arm/mach-exynos/include/mach/uncompress.h
+++ arch/arm/mach-exynos/include/mach/uncompress.h
@@ -38,7 +38,7 @@ static void arch_detect_cpu(void)
 	chip_id &= EXYNOS5_SOC_MASK;
 	chip_id >>= 16;
 
-	if (chip_id == 0x4352)
+	if ((chip_id == 0x4352) || (chip_id = 0xE542))
 		uart_base = (volatile u8 *)EXYNOS5_PA_UART + (S3C_UART_OFFSET * CONFIG_S3C_LOWLEVEL_UART_PORT);
 	else if (chip_id & 0xE543)
 		uart_base = (volatile u8 *)EXYNOS5430_PA_UART + (S3C_UART_OFFSET * CONFIG_S3C_LOWLEVEL_UART_PORT);
