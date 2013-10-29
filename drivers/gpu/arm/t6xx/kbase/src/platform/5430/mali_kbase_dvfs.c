5464c397fc80a2c05b26bc030da6524fa8c8b17f
Ryan Jung (yousub) <yousub.jung@samsung.com>
2013-10-29 11:35:59 +0900

    gpu: t6xx:  pm_domain dt name change
    Change-Id: I2da85a25930217f31b705fb4520472ad4df9c3a1
    Signed-off-by: Ryan Jung (yousub) <yousub.jung@samsung.com>

--- drivers/gpu/arm/t6xx/kbase/src/platform/5430/mali_kbase_dvfs.c
+++ drivers/gpu/arm/t6xx/kbase/src/platform/5430/mali_kbase_dvfs.c
@@ -848,7 +848,7 @@ struct exynos_pm_domain *kbase_platform_get_pm_domain(kbase_device *kbdev)
 	struct device_node *np = NULL;
 	struct exynos_pm_domain *pd_temp, *pd = NULL;
 
-	for_each_compatible_node(np, NULL, "samsung,exynos5430-pd")
+	for_each_compatible_node(np, NULL, "samsung,exynos-pd")
 	{
 		if (!of_device_is_available(np))
 			continue;
