6d905ebf445a761bb0aa233e4ffadf4f823da1ea
Donguk Ryu <du.ryu@samsung.com>
2013-11-01 13:06:09 +0900

    ARM: EXYNOS: modify g2d cci functions for exynos5422
    This patch added g2d cci functions for exynos5422.
    
    Change-Id: Ib6cfc24ee67fd584dd009c7ef2fd85ee0fc7bd6b
    Signed-off-by: Donguk Ryu <du.ryu@samsung.com>

--- arch/arm/mach-exynos/dev-fimg2d.c
+++ arch/arm/mach-exynos/dev-fimg2d.c
@@ -10,66 +10,178 @@
  */
 
 #include <linux/kernel.h>
+#include <linux/dma-mapping.h>
+#include <linux/platform_device.h>
+#include <linux/interrupt.h>
+#include <linux/ioport.h>
+#include <linux/export.h>
 #include <linux/io.h>
 #include <linux/errno.h>
-
-#include <mach/map.h>
 #include <plat/devs.h>
+#include <plat/cpu.h>
 #include <plat/fimg2d.h>
+#include <mach/map.h>
 
-#define EXYNOS5430_PA_SYSREG_G2D	0x124C0000
-#define G2D_USER_CON			0x1000
-
-#define G2DX_SHARED_VAL_MASK		(1 << 8)
-#define G2DX_SHARED_VAL_SHIFT		(8)
-#define G2DX_SHARED_SEL_MASK		(1 << 0)
-#define G2DX_SHARED_SEL_SHIFT		(0)
+#define S5P_PA_FIMG2D_OFFSET	0x02000000
+#define S5P_PA_FIMG2D_3X	(S5P_PA_FIMG2D+S5P_PA_FIMG2D_OFFSET)
 
 static void __iomem *sysreg_g2d_base;
 
-int g2d_cci_snoop_init(void)
+#ifndef CONFIG_OF
+static struct resource s5p_fimg2d_resource[] = {
+	[0] = DEFINE_RES_MEM(S5P_PA_FIMG2D, SZ_4K),
+	[1] = DEFINE_RES_IRQ(IRQ_2D),
+};
+
+struct platform_device s5p_device_fimg2d = {
+	.name		= "s5p-fimg2d",
+	.id		= -1,
+	.num_resources	= ARRAY_SIZE(s5p_fimg2d_resource),
+	.resource	= s5p_fimg2d_resource
+};
+
+static struct fimg2d_platdata default_fimg2d_data __initdata = {
+	.parent_clkname	= "mout_g2d0",
+	.clkname	= "sclk_fimg2d",
+	.gate_clkname	= "fimg2d",
+	.clkrate	= 200 * MHZ,
+};
+
+void __init s5p_fimg2d_set_platdata(struct fimg2d_platdata *pd)
 {
-	sysreg_g2d_base = ioremap(EXYNOS5430_PA_SYSREG_G2D, 0x2000);
-	if (!sysreg_g2d_base) {
-		pr_err("syrreg_g2d_base ioremap is failed\n");
-		return -ENOMEM;
+	struct fimg2d_platdata *npd;
+
+	if (soc_is_exynos4210()) {
+		s5p_fimg2d_resource[0].start = S5P_PA_FIMG2D_3X;
+		s5p_fimg2d_resource[0].end = S5P_PA_FIMG2D_3X + SZ_4K - 1;
 	}
 
+	if (!pd)
+		pd = &default_fimg2d_data;
+
+	npd = s3c_set_platdata(pd, sizeof(struct fimg2d_platdata),
+			&s5p_device_fimg2d);
+	if (!npd)
+		pr_err("%s: fail s3c_set_platdata()\n", __func__);
+}
+#endif
+
+int g2d_cci_snoop_init(int ip_ver)
+{
+	switch (ip_ver) {
+	case IP_VER_G2D_5R:
+		sysreg_g2d_base = ioremap(EXYNOS5430_PA_SYSREG_G2D, 0x2000);
+		if (!sysreg_g2d_base) {
+			pr_err("syrreg_g2d_base ioremap is failed\n");
+			return -ENOMEM;
+		}
+		break;
+
+	case IP_VER_G2D_5H:
+		sysreg_g2d_base = ioremap(EXYNOS5430_PA_SYSREG_G2D, 0x1000);
+		if (!sysreg_g2d_base) {
+			pr_err("syrreg_g2d_base ioremap is failed\n");
+			return -ENOMEM;
+		}
+		break;
+
+	case IP_VER_G2D_5AR:
+		break;
+
+	case IP_VER_G2D_5AR2:
+		sysreg_g2d_base = ioremap(EXYNOS5422_PA_SYSREG_G2D, 0x1000);
+		if (!sysreg_g2d_base) {
+			pr_err("syrreg_g2d_base ioremap is failed\n");
+			return -ENOMEM;
+		}
+		break;
+
+	default:
+		pr_err("syrreg_g2d_base ioremap is failed\n");
+		break;
+	}
 	return 0;
 }
 
-void g2d_cci_snoop_remove(void)
+void g2d_cci_snoop_remove(int ip_ver)
 {
-	iounmap(sysreg_g2d_base);
+	switch (ip_ver) {
+	case IP_VER_G2D_5R:
+	case IP_VER_G2D_5H:
+	case IP_VER_G2D_5AR:
+	case IP_VER_G2D_5AR2:
+		iounmap(sysreg_g2d_base);
+		break;
+	default:
+		pr_err("syrreg_g2d_base iounmap is failed\n");
+		break;
+	}
 }
 
-int g2d_cci_snoop_control(enum g2d_shared_val val, enum g2d_shared_sel sel)
+int g2d_cci_snoop_control(int ip_ver
+		, enum g2d_shared_val val, enum g2d_shared_sel sel)
 {
 	void __iomem *control_reg;
 	unsigned int cfg;
 
-	if ((val >= SHAREABLE_VAL_END)
-		|| (sel >= SHAREABLE_SEL_END)) {
-		pr_err("g2d val or sel are out of range. val:%d, sel:%d\n", val, sel);
+	if ((val >= SHAREABLE_VAL_END) || (sel >= SHAREABLE_SEL_END)) {
+		pr_err("g2d val or sel are out of range. val:%d, sel:%d\n"
+				, val, sel);
 		return -EINVAL;
 	}
 
-	//printk("[%s:%d] val:%d, sel:%d\n", __func__, __LINE__, val, sel);
+	switch (ip_ver) {
+	case IP_VER_G2D_5R:
 
-	control_reg = sysreg_g2d_base + G2D_USER_CON;
+		control_reg = sysreg_g2d_base + EXYNOS5260_G2D_USER_CON;
+
+		if (sel == SHARED_G2D_SEL) {
+			/* disable cci path */
+			cfg = (EXYNOS5260_G2D_ARUSER_SEL |
+					EXYNOS5260_G2D_AWUSER_SEL);
+			writel(cfg, control_reg);
+	}
+
+		control_reg = sysreg_g2d_base + EXYNOS5260_G2D_AXUSER_SEL;
+		if (val == NON_SHAREABLE_PATH) {
+			cfg = EXYNOS5260_G2D_SEL;
+			writel(cfg, control_reg);
+		}
+		break;
+
+	case IP_VER_G2D_5H:
+
+		control_reg = sysreg_g2d_base + EXYNOS5430_G2D_USER_CON;
 
 	cfg = readl(control_reg);
+		cfg &= ~EXYNOS5430_G2DX_SHARED_VAL_MASK;
+		cfg |= val << EXYNOS5430_G2DX_SHARED_VAL_SHIFT;
 
-	cfg &= ~G2DX_SHARED_VAL_MASK;
-	cfg |= val << G2DX_SHARED_VAL_SHIFT;
+		cfg &= ~EXYNOS5430_G2DX_SHARED_SEL_MASK;
+		cfg |= sel << EXYNOS5430_G2DX_SHARED_SEL_SHIFT;
 
-	cfg &= ~G2DX_SHARED_SEL_MASK;
-	cfg |= sel << G2DX_SHARED_SEL_SHIFT;
+		pr_debug("[%s:%d] control_reg :0x%p cfg:0x%x\n"
+				,  __func__, __LINE__, control_reg, cfg);
 
-	//printk("[%s:%d] control_reg:0x%p cfg:0x%x\n"
-	//		, __func__, __LINE__, control_reg, cfg);
+		writel(cfg, control_reg);
+		break;
 
+	case IP_VER_G2D_5AR:
+		/* G2D CCI off */
+		/* cci.c : cci_init() : G2D_BACKBONE_SEL: 0x1 - CCIby-pass ON */
+		break;
+
+	case IP_VER_G2D_5AR2:
+		control_reg = sysreg_g2d_base;
+		/* G2D CCI off */
+		/* G2D_BACKBONE_SEL: 0x1 - CCIby-pass ON */
+		cfg = 0x1;
 	writel(cfg, control_reg);
+		break;
+	default:
+		pr_err("syrreg_g2d_base ioremap is failed\n");
+		break;
+	}
 
 	return 0;
 }
