cef964dfe738a8a6b22ca885fe367bd32b78c28c
Park Bumgyu <bumgyu.park@samsung.com>
2013-11-04 14:05:00 +0900

    ARM: exynos: Support evt0 and evt1 CMU register map by one file
    Change-Id: I93cdc12acc3e6ef1f4387e5db4e5402deb5e01c0
    Signed-off-by: Park Bumgyu <bumgyu.park@samsung.com>

--- drivers/clk/samsung/clk-exynos5430_evt0.c
+++ drivers/clk/samsung/clk-exynos5430_evt0.c
@@ -14,7 +14,7 @@
 #include <linux/clk-provider.h>
 #include <linux/of.h>
 #include <linux/of_address.h>
-#include <mach/regs-clock-exynos5430_evt0.h>
+#include <mach/regs-clock-exynos5430.h>
 
 #include "clk.h"
 #include "clk-pll.h"
