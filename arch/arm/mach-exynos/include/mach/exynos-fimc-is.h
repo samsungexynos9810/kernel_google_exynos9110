a8ff47c33e79fb502ff806120248d58f4e475bc3
Teahyung Kim <tkon.kim@samsung.com>
2013-11-05 19:17:13 +0900

    ARM: EXYNOS: sensor device setup is changed
    Change-Id: I2dda88269d5ea63f58f380ade6a8906e7c8b6862
    Signed-off-by: lim gilyeon <kilyeon.im@samsung.com>

--- arch/arm/mach-exynos/include/mach/exynos-fimc-is.h
+++ arch/arm/mach-exynos/include/mach/exynos-fimc-is.h
@@ -20,14 +20,7 @@
 #endif
 
 #define FIMC_IS_DEV_NAME			"exynos-fimc-is"
-
-#define FIMC_IS_MAX_CAMIF_CLIENTS	2
-#define FIMC_IS_MAX_NAME_LEN		32
-#define FIMC_IS_MAX_GPIO_NUM		32
-#define UART_ISP_SEL			0
-#define UART_ISP_RATIO			1
-
-#define to_fimc_is_plat(d)	(to_platform_device(d)->dev.platform_data)
+#include <linux/platform_device.h>
 
 /* FIMC-IS DVFS LEVEL enum (INT, MIF, I2C) */
 enum FIMC_IS_INT_LV {
@@ -76,299 +69,6 @@ enum FIMC_IS_SCENARIO_ID {
 	FIMC_IS_SN_MAX,
 };
 
-enum exynos_csi_id {
-	CSI_ID_A = 0,
-	CSI_ID_B = 1,
-	CSI_ID_C = 2,
-	CSI_ID_MAX
-};
-
-enum exynos_flite_id {
-	FLITE_ID_A = 0,
-	FLITE_ID_B = 1,
-	FLITE_ID_C = 2,
-	FLITE_ID_END = 3,
-};
-
-enum exynos_sensor_position {
-	SENSOR_POSITION_REAR = 0,
-	SENSOR_POSITION_FRONT
-};
-
-enum exynos_sensor_id {
-	SENSOR_NAME_NOTHING		 = 0,
-	SENSOR_NAME_S5K3H2		 = 1,
-	SENSOR_NAME_S5K6A3		 = 2,
-	SENSOR_NAME_S5K3H5		 = 3,
-	SENSOR_NAME_S5K3H7		 = 4,
-	SENSOR_NAME_S5K3H7_SUNNY	 = 5,
-	SENSOR_NAME_S5K3H7_SUNNY_2M	 = 6,
-	SENSOR_NAME_S5K6B2		 = 7,
-	SENSOR_NAME_S5K3L2		 = 8,
-	SENSOR_NAME_S5K4E5		 = 9,
-	SENSOR_NAME_S5K2P2		 = 10,
-	SENSOR_NAME_IMX134               = 36,
-
-	SENSOR_NAME_IMX135		 = 101, /* 101 ~ 200 Sony sensors */
-
-	SENSOR_NAME_SR261		 = 201, /* 201 ~ 300 Other vendor sensors */
-
-	SENSOR_NAME_END,
-	SENSOR_NAME_CUSTOM		 = 301,
-};
-
-/* Sensor configurations are decided through JA, HA, VA projects with TANGO. */
-enum is_s5k3h2_config_enum {
-	S5K3H2_CONFIG_DEFAULT = 0,
-};
-
-enum is_s5k6a3_config_enum{
-	S5K6A3_CONFIG_DEFAULT = 0,
-};
-
-enum is_s5k3h5_config_enum{
-	S5K3H5_CONFIG_DEFAULT = 0,
-};
-
-enum is_s5k3h7_config_enum{
-	S5K3H7_CONFIG_DEFAULT = 0,
-};
-
-enum is_s5k3h7sunny_config_enum{
-	S5K3H7_SUNNY_CONFIG_DEFAULT = 0,
-};
-
-enum is_s5k3h7sunny2m_config_enum {
-	S5K3H7_SUNNY_2M_CONFIG_DEFAULT = 0,
-};
-
-enum is_s5k6b2_config_enum {
-	S5K6B2_CONFIG_1936X1090_30 = 0,
-	S5K6B2_CONFIG_1936X1090_24 = 1,
-	S5K6B2_CONFIG_BINNING_2X2 = 2,
-};
-
-enum is_s5k3l2_config_enum{/* Below configurations are decided through JA, HA, VA projects with TANGO. */
-	S5K3L2_CONFIG_4144X3106_30 = 0, /*  4:3 Single still preview/capture, Single video (snapshot), Dual still capture */
-	S5K3L2_CONFIG_4144X2332_30 = 1, /*  16:9 Single still preview/capture, Single video (snapshot), Dual still capture */
-	S5K3L2_CONFIG_2072X1554_24 = 2, /*  4:3 Dual still preview, Dual video */
-	S5K3L2_CONFIG_2072X1166_24 = 3, /*  16:9 Dual still preview, Dual video */
-	S5K3L2_CONFIG_1040X584_120 = 4, /*  Slow video motion 1/4, 1/8 */
-	S5K3L2_CONFIG_2072X1166_60 = 5, /*  Slow video motion 1/2, FHD recording 60fps, Golf shot 60fps */
-};
-
-enum is_s5k4e5_config_enum{
-	S5K4E5_CONFIG_DEFAULT = 0,
-};
-
-enum is_s5k2p2_config_enum{
-	S5K2P2_CONFIG_DEFAULT = 0,
-};
-
-enum is_imx135_config_enum{
-	IMX135_CONFIG_4144X3106_30 = 0, /* 4:3 Single still preview/capture, Single video (snapshot), Dual still capture */
-	IMX135_CONFIG_4144X2332_30 = 1, /* 16:9 Single still preview/capture, Single video (snapshot), Dual still capture */
-	IMX135_CONFIG_1936X1450_24 = 2, /* 4:3 Dual still preview, Dual video */
-	IMX135_CONFIG_1936X1090_24 = 3, /* 16:9 Dual still preview, Dual video */
-	IMX135_CONFIG_1024X576_120 = 4, /* Slow video motion 1/4, 1/8 */
-	IMX135_CONFIG_2072X1166_60 = 5, /* Slow video motion 1/2, FHD recording 60fps, Golf shot 60fps */
-};
-
-enum is_sr261_config_enum{
-	SR261_CONFIG_DEFAULT = 0,
-};
-
-struct exynos_sensor_power_info {
-	char cam_core[FIMC_IS_MAX_NAME_LEN];
-	char cam_io_myself[FIMC_IS_MAX_NAME_LEN];
-	char cam_io_peer[FIMC_IS_MAX_NAME_LEN];
-	char cam_af[FIMC_IS_MAX_NAME_LEN];
-
-	char core[FIMC_IS_MAX_NAME_LEN];
-	char io[FIMC_IS_MAX_NAME_LEN];
-	char analog[FIMC_IS_MAX_NAME_LEN];
-	char af[FIMC_IS_MAX_NAME_LEN];
-};
-
-enum actuator_name {
-	ACTUATOR_NAME_AD5823	= 1,
-	ACTUATOR_NAME_DWXXXX	= 2,
-	ACTUATOR_NAME_AK7343	= 3,
-	ACTUATOR_NAME_HYBRIDVCA	= 4,
-	ACTUATOR_NAME_LC898212	= 5,
-	ACTUATOR_NAME_WV560	= 6,
-	ACTUATOR_NAME_AK7345	= 7,
-	ACTUATOR_NAME_NOTHING	= 100,
-	ACTUATOR_NAME_END
-};
-
-enum flash_drv_name {
-	FLADRV_NAME_KTD267	= 1,	/* Gpio type(Flash mode, Movie/torch mode) */
-	FLADRV_NAME_AAT1290A	= 2,
-	FLADRV_NAME_MAX77693	= 3,
-	FLADRV_NAME_NOTHING	= 100,
-	FLADRV_NAME_END
-};
-
-enum from_name {
-	FROMDRV_NAME_W25Q80BW	= 1,	/* Spi type */
-	FROMDRV_NAME_FM25M16A	= 2,	/* Spi type */
-	FROMDRV_NAME_NOTHING	= 100,
-};
-
-enum sensor_peri_type {
-	SE_I2C,
-	SE_SPI,
-	SE_GPIO,
-	SE_MPWM,
-	SE_ADC,
-	SE_NULL
-};
-
-struct i2c_type {
-	u32 channel;
-	u32 slave_address;
-	u32 speed;
-};
-
-struct spi_type {
-	u32 channel;
-};
-
-struct gpio_type {
-	u32 first_gpio_port_no;
-	u32 second_gpio_port_no;
-};
-
-union sensor_peri_format {
-	struct i2c_type i2c;
-	struct spi_type spi;
-	struct gpio_type gpio;
-};
-
-struct sensor_protocol {
-	u32 product_name;
-	enum sensor_peri_type peri_type;
-	union sensor_peri_format peri_setting;
-};
-
-enum exynos_sensor_channel {
-	SENSOR_CONTROL_I2C0	 = 0,
-	SENSOR_CONTROL_I2C1	 = 1,
-	SENSOR_CONTROL_I2C2	 = 2
-};
-
-enum pin_type {
-	PIN_GPIO	= 1,
-	PIN_REGULATOR	= 2,
-};
-
-enum gpio_act {
-	GPIO_PULL_NONE = 0,
-	GPIO_OUTPUT,
-	GPIO_INPUT,
-	GPIO_RESET
-};
-
-struct gpio_set {
-	enum pin_type pin_type;
-	unsigned int pin;
-	char name[FIMC_IS_MAX_NAME_LEN];
-	unsigned int value;
-	enum gpio_act act;
-	enum exynos_flite_id flite_id;
-	int count;
-};
-
-struct exynos_sensor_gpio_info {
-	struct gpio_set cfg[FIMC_IS_MAX_GPIO_NUM];
-	struct gpio_set reset_myself;
-	struct gpio_set reset_peer;
-	struct gpio_set power;
-
-	struct gpio_set power_core;
-	struct gpio_set power_io;
-	struct gpio_set power_analog;
-	struct gpio_set power_af;
-};
-
-struct fimc_is_gpio_info {
-	int gpio_main_rst;
-	int gpio_main_sda;
-	int gpio_main_scl;
-	int gpio_main_mclk;
-	int gpio_main_flash_en;
-	int gpio_main_flash_torch;
-
-	int gpio_vt_rst;
-	int gpio_vt_sda;
-	int gpio_vt_scl;
-	int gpio_vt_mclk;
-
-	int gpio_spi_clk;
-	int gpio_spi_csn;
-	int gpio_spi_miso;
-	int gpio_spi_mosi;
-
-	int gpio_uart_txd;
-	int gpio_uart_rxd;
-	int gpio_comp_en;
-	int gpio_comp_rst;
-};
-
-struct platform_device;
-
- /**
-  * struct exynos5_fimc_is_sensor_info	- image sensor information required for host
-  *			       interace configuration.
- */
-struct exynos_fimc_is_sensor_info {
-	const char *sensor_name;
-	enum exynos_sensor_position sensor_position;
-	enum exynos_sensor_id sensor_id;
-	enum exynos_csi_id clk_src;
-	enum exynos_csi_id csi_id;
-	enum exynos_flite_id flite_id;
-	enum exynos_sensor_channel i2c_channel;
-	struct exynos_sensor_power_info sensor_power;
-	struct exynos_sensor_gpio_info sensor_gpio;
-
-	int max_width;
-	int max_height;
-	int max_frame_rate;
-
-	int mipi_lanes;     /* MIPI data lanes */
-	int mipi_settle;    /* MIPI settle */
-	int mipi_align;     /* MIPI data align: 24/32 */
-
-	u32 sensor_slave_address;
-	enum actuator_name actuator_id;
-	enum exynos_sensor_channel actuator_i2c;
-	enum flash_drv_name flash_id;
-	enum sensor_peri_type flash_peri_type;
-	u32 flash_first_gpio;
-	u32 flash_second_gpio;
-};
-
-struct sensor_open_extended {
-	struct sensor_protocol sensor_con;
-	struct sensor_protocol actuator_con;
-	struct sensor_protocol flash_con;
-	struct sensor_protocol from_con;
-
-	u32 mclk;
-	u32 mipi_lane_num;
-	u32 mipi_settle_line;
-	u32 mipi_speed;
-	/* Skip setfile loading when fast_open_sensor is not 0 */
-	u32 fast_open_sensor;
-	/* Activatiing sensor self calibration mode (6A3) */
-	u32 self_calibration_mode;
-	/* This field is to adjust I2c clock based on ACLK200 */
-	/* This value is varied in case of rev 0.2 */
-	u32 I2CSclk;
-};
-
 /*
  * struct exynos_fimc_is_subip_info
  *   This info describes what and how many subip fimc-is has.
@@ -395,19 +95,11 @@ struct exynos_fimc_is_subip_info {
 */
 struct exynos_platform_fimc_is {
 	int	hw_ver;
-	struct exynos_fimc_is_sensor_info *sensor_info[FIMC_IS_MAX_CAMIF_CLIENTS];
-	struct exynos_sensor_gpio_info *gpio_info;
-	struct fimc_is_gpio_info *_gpio_info;
 	struct exynos_fimc_is_subip_info *subip_info;
-	int	flag_power_on[FLITE_ID_END];
 	int	(*cfg_gpio)(struct platform_device *pdev, int channel, bool flag_on);
 	int	(*clk_cfg)(struct platform_device *pdev);
 	int	(*clk_on)(struct platform_device *pdev);
 	int	(*clk_off)(struct platform_device *pdev);
-	int	(*sensor_clock_on)(struct platform_device *pdev, u32 source);
-	int	(*sensor_clock_off)(struct platform_device *pdev, u32 source);
-	int	(*sensor_power_on)(struct platform_device *pdev, int sensor_id);
-	int	(*sensor_power_off)(struct platform_device *pdev, int sensor_id);
 	int	(*print_cfg)(struct platform_device *pdev, u32 channel);
 
 	/* These fields are to return qos value for dvfs scenario */
@@ -421,9 +113,14 @@ struct exynos_platform_fimc_is {
 
 extern void exynos_fimc_is_set_platdata(struct exynos_platform_fimc_is *pd);
 
-/* defined by architecture to configure gpio */
-extern int exynos5_fimc_is_cfg_gpio(struct platform_device *pdev,
-					int channel, bool flag_on);
+int fimc_is_set_parent_dt(struct platform_device *pdev,
+	const char *child, const char *parent);
+struct clk *fimc_is_get_parent_dt(struct platform_device *pdev,
+	const char *child);
+int fimc_is_set_rate_dt(struct platform_device *pdev,
+	const char *conid, unsigned int rate);
+unsigned int  fimc_is_get_rate_dt(struct platform_device *pdev,
+	const char *conid);
 
 /* platform specific clock functions */
 #if defined(CONFIG_ARCH_EXYNOS4)
