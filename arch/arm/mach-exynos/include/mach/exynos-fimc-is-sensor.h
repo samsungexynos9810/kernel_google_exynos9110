e070f48d04013e3de7cf4e234c1c00bc18dcdfbf
Teahyung Kim <tkon.kim@samsung.com>
2013-11-06 16:09:15 +0900

    Revert "ARCH: ARM: EXYNOS: sensor device setup is changed"
    This reverts commit 31b9b24d95233064415f45ed19df0224838af56c.
    
    Change-Id: Ia3fff88816a95d6b6cfd7d2a69782b3dde6b3bb0
    Signed-off-by: Teahyung Kim <tkon.kim@samsung.com>

--- arch/arm/mach-exynos/include/mach/exynos-fimc-is-sensor.h
+++ arch/arm/mach-exynos/include/mach/exynos-fimc-is-sensor.h
@@ -15,167 +15,8 @@
 
 #include <mach/exynos-fimc-is.h>
 
-#define FIMC_IS_SENSOR_DEV_NAME "exynos5-fimc-is-sensor"
-#define FIMC_IS_PINNAME_LEN 32
-
-enum exynos5_csi_id {
-	CSI_ID_A = 0,
-	CSI_ID_B = 1,
-	CSI_ID_C = 2,
-	CSI_ID_MAX
-};
-
-enum exynos5_flite_id {
-	FLITE_ID_A = 0,
-	FLITE_ID_B = 1,
-	FLITE_ID_C = 2,
-	FLITE_ID_END = 3,
-};
-
-enum exynos5_sensor_position {
-	SENSOR_POSITION_REAR = 0,
-	SENSOR_POSITION_FRONT
-};
-
-enum exynos5_sensor_id {
-	SENSOR_NAME_NOTHING		 = 0,
-	SENSOR_NAME_S5K3H2		 = 1,
-	SENSOR_NAME_S5K6A3		 = 2,
-	SENSOR_NAME_S5K3H5		 = 3,
-	SENSOR_NAME_S5K3H7		 = 4,
-	SENSOR_NAME_S5K3H7_SUNNY	 = 5,
-	SENSOR_NAME_S5K3H7_SUNNY_2M	 = 6,
-	SENSOR_NAME_S5K6B2		 = 7,
-	SENSOR_NAME_S5K3L2		 = 8,
-	SENSOR_NAME_S5K4E5		 = 9,
-	SENSOR_NAME_S5K2P2		 = 10,
-
-	SENSOR_NAME_IMX135		 = 101, /* 101 ~ 200 Sony sensors */
-
-	SENSOR_NAME_SR261		 = 201, /* 201 ~ 300 Other vendor sensors */
-
-	SENSOR_NAME_END,
-	SENSOR_NAME_CUSTOM		 = 301,
-};
-
-enum actuator_name {
-	ACTUATOR_NAME_AD5823	= 1,
-	ACTUATOR_NAME_DWXXXX	= 2,
-	ACTUATOR_NAME_AK7343	= 3,
-	ACTUATOR_NAME_HYBRIDVCA	= 4,
-	ACTUATOR_NAME_LC898212	= 5,
-	ACTUATOR_NAME_WV560	= 6,
-	ACTUATOR_NAME_AK7345	= 7,
-	ACTUATOR_NAME_NOTHING	= 100,
-	ACTUATOR_NAME_END
-};
-
-enum flash_drv_name {
-	FLADRV_NAME_KTD267	= 1,	/* Gpio type(Flash mode, Movie/torch mode) */
-	FLADRV_NAME_AAT1290A	= 2,
-	FLADRV_NAME_MAX77693	= 3,
-	FLADRV_NAME_AS3643	= 4,
-	FLADRV_NAME_NOTHING	= 100,
-	FLADRV_NAME_END
-};
-
-enum from_name {
-	FROMDRV_NAME_W25Q80BW	= 1,	/* Spi type */
-	FROMDRV_NAME_FM25M16A	= 2,	/* Spi type */
-	FROMDRV_NAME_NOTHING	= 100,
-};
-
-enum sensor_peri_type {
-	SE_I2C,
-	SE_SPI,
-	SE_GPIO,
-	SE_MPWM,
-	SE_ADC,
-	SE_NULL
-};
-
-struct i2c_type {
-	u32 channel;
-	u32 slave_address;
-	u32 speed;
-};
-
-struct spi_type {
-	u32 channel;
-};
-
-struct gpio_type {
-	u32 first_gpio_port_no;
-	u32 second_gpio_port_no;
-};
-
-union sensor_peri_format {
-	struct i2c_type i2c;
-	struct spi_type spi;
-	struct gpio_type gpio;
-};
-
-struct sensor_protocol {
-	u32 product_name;
-	enum sensor_peri_type peri_type;
-	union sensor_peri_format peri_setting;
-};
-
-enum exynos5_sensor_channel {
-	SENSOR_CONTROL_I2C0	 = 0,
-	SENSOR_CONTROL_I2C1	 = 1,
-	SENSOR_CONTROL_I2C2	 = 2
-};
-
-struct sensor_open_extended {
-	struct sensor_protocol sensor_con;
-	struct sensor_protocol actuator_con;
-	struct sensor_protocol flash_con;
-	struct sensor_protocol from_con;
-
-	u32 mclk;
-	u32 mipi_lane_num;
-	u32 mipi_settle_line;
-	u32 mipi_speed;
-	/* Skip setfile loading when fast_open_sensor is not 0 */
-	u32 fast_open_sensor;
-	/* Activatiing sensor self calibration mode (6A3) */
-	u32 self_calibration_mode;
-	/* This field is to adjust I2c clock based on ACLK200 */
-	/* This value is varied in case of rev 0.2 */
-	u32 I2CSclk;
-};
-
-#define GPIO_SCENARIO_ON		0
-#define GPIO_SCENARIO_OFF		1
-#define GPIO_SCENARIO_MAX		2
-#define GPIO_CTRL_MAX			10
-
-#define SENSOR_SCENARIO_NORMAL		0
-#define SENSOR_SCENARIO_VISION		1
-#define SENSOR_SCENARIO_MAX 		10
-
-enum pin_act {
-	PIN_PULL_NONE = 0,
-	PIN_OUTPUT_HIGH,
-	PIN_OUTPUT_LOW,
-	PIN_INPUT,
-	PIN_RESET,
-	PIN_FUNCTION,
-	PIN_REGULATOR_ON,
-	PIN_REGULATOR_OFF,
-	PIN_END
-};
-
-struct exynos_sensor_pin {
-	int pin;
-	u32 value;
-	char name[FIMC_IS_PINNAME_LEN];
-	enum pin_act act;
-};
-
 /*
- * struct exynos_platform_fimc_is_sensor - platform data for exynos_sensor driver
+ * struct exynos_fimc_is_sensor_platform_data - platform data for exynos_sensor driver
  * @irq: GPIO getting the irq pin of exynos_sensor
  * @gpio_rst: GPIO driving the reset pin of exynos_sensor
  * @enable_rst: the pin state when reset pin is enabled
@@ -184,39 +25,17 @@ struct exynos_sensor_pin {
  *			to be called after enabling and before disabling
  *			the exynos_sensor device supply regulators
  */
-struct exynos_platform_fimc_is_sensor {
-	int (*gpio_cfg)(struct platform_device *pdev, u32 scenario, u32 enable);
-	int (*iclk_cfg)(struct platform_device *pdev, u32 scenario, u32 channel);
-	int (*iclk_on)(struct platform_device *pdev,u32 scenario, u32 channel);
-	int (*iclk_off)(struct platform_device *pdev, u32 scenario, u32 channel);
-	int (*mclk_on)(struct platform_device *pdev, u32 scenario, u32 channel);
-	int (*mclk_off)(struct platform_device *pdev, u32 scenario, u32 channel);
-	struct exynos_sensor_pin pin_ctrls[SENSOR_SCENARIO_MAX][GPIO_SCENARIO_MAX][GPIO_CTRL_MAX];
-	u32 scenario;
-	u32 mclk_ch;
-	u32 csi_ch;
-	u32 flite_ch;
-	u32 i2c_ch;
-	u32 i2c_addr;
+struct exynos_fimc_is_sensor_platform_data {
+	int (*clk_on)(struct device *pdev, int sensor_id);
+	int (*clk_off)(struct device *pdev, int sensor_id);
+	int (*set_power)(struct device *dev, int on);
+	struct exynos5_sensor_gpio_info *gpio_info;
+	int irq;
+	int gpio_rst;
+	bool enable_rst;
 };
 
-extern int exynos_fimc_is_sensor_pins_cfg(struct platform_device *pdev,
-	u32 scenario,
-	u32 enable);
-extern int exynos_fimc_is_sensor_iclk_cfg(struct platform_device *pdev,
-	u32 scenario,
-	u32 channel);
-extern int exynos_fimc_is_sensor_iclk_on(struct platform_device *pdev,
-	u32 scenario,
-	u32 channel);
-extern int exynos_fimc_is_sensor_iclk_off(struct platform_device *pdev,
-	u32 scenario,
-	u32 channel);
-extern int exynos_fimc_is_sensor_mclk_on(struct platform_device *pdev,
-	u32 scenario,
-	u32 channel);
-extern int exynos_fimc_is_sensor_mclk_off(struct platform_device *pdev,
-	u32 scenario,
-	u32 channel);
+extern int exynos5_fimc_is_sensor_clk_on(struct device *pdev, int sensor_id);
+extern int exynos5_fimc_is_sensor_clk_off(struct device *pdev, int sensor_id);
 
 #endif /* MEDIA_EXYNOS_SENSOR_H */
