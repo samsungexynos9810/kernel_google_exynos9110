806f4373d8ff6b4bc549053773ac20ae814d4e35
Teahyung Kim <tkon.kim@samsung.com>
2013-11-06 21:07:42 +0900

    ARM: EXYNOS: added clock gating info to exynos-fimc-is header
    Change-Id: I1ade404edbf3a4868c46ff0d7ffca79be0dcd632
    Signed-off-by: Teahyung Kim <tkon.kim@samsung.com>

--- arch/arm/mach-exynos/include/mach/exynos-fimc-is.h
+++ arch/arm/mach-exynos/include/mach/exynos-fimc-is.h
@@ -69,6 +69,33 @@ enum FIMC_IS_SCENARIO_ID {
 	FIMC_IS_SN_MAX,
 };
 
+enum FIMC_IS_CLK_GATE {
+	FIMC_IS_GATE_3AA1_IP,
+	FIMC_IS_GATE_ISP_IP,
+	FIMC_IS_GATE_DRC_IP,
+	FIMC_IS_GATE_SCC_IP,
+	FIMC_IS_GATE_ODC_IP,
+	FIMC_IS_GATE_DIS_IP,
+	FIMC_IS_GATE_3DNR_IP,
+	FIMC_IS_GATE_SCP_IP,
+	FIMC_IS_GATE_FD_IP,
+	FIMC_IS_GATE_3AA0_IP,
+	FIMC_IS_CLK_GATE_MAX,
+};
+
+enum FIMC_IS_GRP {
+	FIMC_IS_GRP_3A0,
+	FIMC_IS_GRP_3A1,
+	FIMC_IS_GRP_ISP,
+	FIMC_IS_GRP_DIS,
+	FIMC_IS_GRP_MAX,
+};
+
+enum FIMC_IS_CLK_GATE_USR_SCENARIO {
+	CLK_GATE_NOT_FULL_BYPASS_SN = 1,
+	CLK_GATE_DIS_SN,
+};
+
 /*
  * struct exynos_fimc_is_subip_info
  *   This info describes what and how many subip fimc-is has.
@@ -86,6 +113,44 @@ struct exynos_fimc_is_subip_info {
 	u32 num_of_3dnr;
 	u32 num_of_scp;
 	u32 num_of_fd;
+};
+
+/*
+ * struct exynos_fimc_is_clk_gate_group
+ * 	This struct is for host clock gating.
+ * 	It decsribes register, masking bit info and other control for each group.
+ *	If you uses host clock gating, You must define this struct in exynos_fimc_is_clk_gate_info.
+ */
+struct exynos_fimc_is_clk_gate_group {
+	u32	mask_clk_on_org;	/* masking value in clk on */
+	u32	mask_clk_on_mod;	/* masking value in clk on */
+	u32	mask_clk_off_self_org;	/* masking value in clk off(not depend) original */
+	u32	mask_clk_off_self_mod;	/* masking value in clk off(not depend) final */
+	u32	mask_clk_off_depend;	/* masking value in clk off(depend on other grp) */
+	u32	mask_cond_for_depend;	/* masking group having dependancy for other */
+};
+
+/*
+ * struct exynos_fimc_is_clk_gate_info
+ * 	This struct is for host clock gating.
+ * 	It has exynos_fimc_is_clk_gate_group to control each group's clk gating.
+ * 	And it has function pointer to include user scenario masking
+ */
+struct exynos_fimc_is_clk_gate_info {
+	const char* gate_str[FIMC_IS_CLK_GATE_MAX];			/* register adr for gating */
+	struct exynos_fimc_is_clk_gate_group groups[FIMC_IS_GRP_MAX];
+	/* You must set this function pointer (on/off) */
+	int (*clk_on_off)(u32 clk_gate_id, bool is_on);
+	/*
+	 * if there are specific scenarios for clock gating,
+	 * You can define user function.
+	 * user_scenario_id will be in
+	 */
+	int (*user_clk_gate)(u32 group_id,
+			bool is_on,
+			u32 user_scenario_id,
+			unsigned long msk_state,
+			struct exynos_fimc_is_clk_gate_info *gate_info);
 };
 
 /**
@@ -109,6 +174,9 @@ struct exynos_platform_fimc_is {
 	int	(*get_int_qos)(int scenario_id);
 	int	(*get_mif_qos)(int scenario_id);
 	int	(*get_i2c_qos)(int scenario_id);
+
+	/* For host clock gating */
+	struct exynos_fimc_is_clk_gate_info *gate_info;
 };
 
 extern void exynos_fimc_is_set_platdata(struct exynos_platform_fimc_is *pd);
@@ -158,6 +226,12 @@ extern int exynos5430_fimc_is_clk_on(struct platform_device *pdev);
 extern int exynos5430_fimc_is_clk_off(struct platform_device *pdev);
 extern int exynos5430_fimc_is_sensor_clk_on(struct platform_device *pdev, u32 source);
 extern int exynos5430_fimc_is_sensor_clk_off(struct platform_device *pdev, u32 source);
+extern int exynos5430_fimc_is_set_user_clk_gate(u32 group_id,
+		bool is_on,
+		u32 user_scenario_id,
+		unsigned long msk_state,
+		struct exynos_fimc_is_clk_gate_info *gate_info);
+extern int exynos5430_fimc_is_clk_gate(	u32 clk_gate_id, bool is_on);
 #endif
 extern int exynos5_fimc_is_sensor_power_on(struct platform_device *pdev, int sensor_id);
 extern int exynos5_fimc_is_sensor_power_off(struct platform_device *pdev, int sensor_id);
