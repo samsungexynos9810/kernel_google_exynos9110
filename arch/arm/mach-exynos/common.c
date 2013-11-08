a128ff672c44dc170d2a1edc99773c23c6554d18
Jiyun Kim <jiyun83.kim@samsung.com>
2013-11-08 19:26:18 +0900

    ARM: EXYNOS5422: add PPMU and CDREX map
    Change-Id: Ifbf309ab35f9e7a831e0287117966a8e186080b6
    Signed-off-by: Jiyun Kim <jiyun83.kim@samsung.com>

--- arch/arm/mach-exynos/common.c
+++ arch/arm/mach-exynos/common.c
@@ -275,7 +275,38 @@ static struct map_desc exynos5422_iodesc[] __initdata = {
 		.pfn		= __phys_to_pfn(EXYNOS5422_PA_SYSRAM_NS),
 		.length		= SZ_4K,
 		.type		= MT_DEVICE,
+	}, {
+		.virtual    = (unsigned long)S5P_VA_PPMU_DREX0_0,
+		.pfn        = __phys_to_pfn(EXYNOS5422_PA_PPMU_DREX0_0),
+		.length     = SZ_8K,
+		.type       = MT_DEVICE,
+	}, {
+		.virtual    = (unsigned long)S5P_VA_PPMU_DREX0_1,
+		.pfn        = __phys_to_pfn(EXYNOS5422_PA_PPMU_DREX0_1),
+		.length     = SZ_8K,
+		.type       = MT_DEVICE,
+	}, {
+		.virtual    = (unsigned long)S5P_VA_PPMU_DREX1_0,
+		.pfn        = __phys_to_pfn(EXYNOS5422_PA_PPMU_DREX1_0),
+		.length     = SZ_8K,
+		.type       = MT_DEVICE,
+	}, {
+		.virtual    = (unsigned long)S5P_VA_PPMU_DREX1_1,
+			.pfn        = __phys_to_pfn(EXYNOS5422_PA_PPMU_DREX1_1),
+			.length     = SZ_8K,
+			.type       = MT_DEVICE,
+	}, {
+		.virtual    = (unsigned long)S5P_VA_DREXI_0,
+			.pfn        = __phys_to_pfn(EXYNOS5_PA_DREXI_0),
+			.length     = SZ_64K,
+			.type       = MT_DEVICE,
+	}, {
+		.virtual    = (unsigned long)S5P_VA_DREXI_1,
+			.pfn        = __phys_to_pfn(EXYNOS5_PA_DREXI_1),
+			.length     = SZ_64K,
+			.type       = MT_DEVICE,
 	},
+
 };
 
 static struct map_desc exynos5_iodesc[] __initdata = {
