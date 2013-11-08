Trustonic/linux

mmc: dw_mmc: add aggressive clock gating
This patch reduces the power comsumption aggressively
when I/O is idle.

Change-Id: I88f6650bf1816e1c78faaa8b6f12d1587ab7ed30
Signed-off-by: Seungwon Jeon <tgih.jun@samsung.com>

include/linux/mmc/dw_mmc.h
@@ -130,6 +130,8 @@ struct dw_mci {
 	bool			stop_snd;
 	struct clk		*hclk;
 	struct clk		*cclk;
+	atomic_t		cclk_cnt;
+	atomic_t		hclk_cnt;
 	struct workqueue_struct	*card_workqueue;
 
 	/* DMA interface members*/

samsungexynos9810/kernel_google_exynos9110

include/linux/mmc/dw_mmc.h
@@ -203,6 +203,8 @@ struct dw_mci {
 	void			*priv;
 	struct clk		*biu_clk;
 	struct clk		*ciu_clk;
+	atomic_t		biu_clk_cnt;
+	atomic_t		ciu_clk_cnt;
 	struct dw_mci_slot	*slot[MAX_MCI_SLOTS];
 
 	/* FIFO push and pull */