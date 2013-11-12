678b2a0fa7d6573254138e705cba8d6346e72154
Suh Seungchull <sc.suh@samsung.com>
2013-11-12 20:47:48 +0900

    pm: devfreq: MIF frequency of EVT0 is changed to 206MHz
    Change-Id: I06a74b1ddaaeabdc2ad955503f1733f31837d1b5
    Signed-off-by: Suh Seungchull <sc.suh@samsung.com>

--- drivers/devfreq/exynos5430_bus_mif.c
+++ drivers/devfreq/exynos5430_bus_mif.c
@@ -401,7 +401,7 @@ static struct devfreq_simple_ondemand_data exynos5_devfreq_mif_governor_data = {
 };
 
 static struct exynos_devfreq_platdata exynos5430_qos_mif = {
-	.default_qos		= 138000,
+	.default_qos		= 206000,
 };
 
 static struct ppmu_info ppmu_mif[] = {
