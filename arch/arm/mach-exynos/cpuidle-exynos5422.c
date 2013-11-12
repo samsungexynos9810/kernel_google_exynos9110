1bd05c3123242b380101117c5b4a20f99b1064f0
Ohkyoung Kwon <ohkyoung.kwon@samsung.com>
2013-11-12 09:19:02 +0900

    ARM: EXYNOS: Add cpu idle function for c1 and c2
    Change-Id: I1c67c1f94a3f820841f88bbece7b10481050cc00
    Signed-off-by: Ohkyoung Kwon <ohkyoung.kwon@samsung.com>

---
+++ arch/arm/mach-exynos/cpuidle-exynos5422.c
@@ -0,0 +1,236 @@
+/* linux/arch/arm/mach-exynos/cpuidle.c
+ *
+ * Copyright (c) 2011 Samsung Electronics Co., Ltd.
+ *		http://www.samsung.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+*/
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/cpuidle.h>
+#include <linux/cpu_pm.h>
+#include <linux/io.h>
+#include <linux/export.h>
+#include <linux/time.h>
+#include <linux/serial_core.h>
+#include <linux/platform_device.h>
+#include <linux/gpio.h>
+#include <linux/suspend.h>
+#include <linux/clk.h>
+#include <linux/fb.h>
+#include <linux/cpu.h>
+#include <linux/pm_qos.h>
+
+#include <asm/proc-fns.h>
+#include <asm/smp_scu.h>
+#include <asm/suspend.h>
+#include <asm/unified.h>
+#include <asm/cputype.h>
+#include <asm/cacheflush.h>
+#include <asm/system_misc.h>
+#include <asm/tlbflush.h>
+
+#include <mach/regs-pmu.h>
+#include <mach/regs-clock.h>
+#include <mach/pmu.h>
+#include <mach/smc.h>
+#include <plat/pm.h>
+#include <plat/cpu.h>
+#include <plat/devs.h>
+#include <plat/regs-serial.h>
+#include <plat/gpio-cfg.h>
+#include <plat/gpio-core.h>
+#include <plat/usb-phy.h>
+#include <plat/clock.h>
+
+#define REG_DIRECTGO_ADDR	(S5P_VA_SYSRAM_NS + 0x24)
+#define REG_DIRECTGO_FLAG	(S5P_VA_SYSRAM_NS + 0x20)
+
+#define EXYNOS_CHECK_DIRECTGO	0xFCBA0D10
+#define EXYNOS_CHECK_LPA	0xABAD0000
+
+static int exynos_enter_idle(struct cpuidle_device *dev,
+			struct cpuidle_driver *drv,
+			      int index);
+#if defined (CONFIG_EXYNOS_CPUIDLE_C2)
+static int exynos_enter_c2(struct cpuidle_device *dev,
+				 struct cpuidle_driver *drv,
+				 int index);
+#endif
+
+static struct cpuidle_state exynos5_cpuidle_set[] __initdata = {
+	[0] = {
+		.enter			= exynos_enter_idle,
+		.exit_latency		= 1,
+		.target_residency	= 1000,
+		.flags			= CPUIDLE_FLAG_TIME_VALID,
+		.name			= "C1",
+		.desc			= "ARM clock gating(WFI)",
+	},
+#if defined (CONFIG_EXYNOS_CPUIDLE_C2)
+	[1] = {
+		.enter			= exynos_enter_c2,
+		.exit_latency		= 30,
+		.target_residency	= 1000,
+		.flags			= CPUIDLE_FLAG_TIME_VALID,
+		.name			= "C2",
+		.desc			= "ARM power down",
+	},
+#endif
+};
+
+static DEFINE_PER_CPU(struct cpuidle_device, exynos_cpuidle_device);
+
+static struct cpuidle_driver exynos_idle_driver = {
+	.name		= "exynos_idle",
+	.owner		= THIS_MODULE,
+};
+
+#if defined (CONFIG_EXYNOS_CPUIDLE_C2)
+static int c2_finisher(unsigned long flags)
+{
+	exynos_smc(SMC_CMD_SAVE, OP_TYPE_CORE, SMC_POWERSTATE_IDLE, 0);
+	exynos_smc(SMC_CMD_SHUTDOWN, OP_TYPE_CORE, SMC_POWERSTATE_IDLE, 0);
+	/*
+	 * Secure monitor disables the SMP bit and takes the CPU out of the
+	 * coherency domain.
+	 */
+	local_flush_tlb_all();
+	return 1;
+}
+#endif
+
+static int exynos_enter_idle(struct cpuidle_device *dev,
+				struct cpuidle_driver *drv,
+				int index)
+{
+	struct timeval before, after;
+	int idle_time;
+
+	local_irq_disable();
+	do_gettimeofday(&before);
+
+	cpu_do_idle();
+
+	do_gettimeofday(&after);
+	local_irq_enable();
+	idle_time = (after.tv_sec - before.tv_sec) * USEC_PER_SEC +
+		    (after.tv_usec - before.tv_usec);
+
+	dev->last_residency = idle_time;
+	return index;
+}
+
+#if defined (CONFIG_EXYNOS_CPUIDLE_C2)
+static int exynos_enter_c2(struct cpuidle_device *dev,
+				struct cpuidle_driver *drv,
+				int index)
+{
+	struct timeval before, after;
+	int idle_time, ret = 0;
+	unsigned int cpuid = smp_processor_id(), cpu_offset = 0;
+	unsigned int value;
+
+	/* KFC don't use C2 state */
+	if (cpuid < 4)
+		return exynos_enter_idle(dev, drv, 0);
+
+	local_irq_disable();
+	do_gettimeofday(&before);
+
+	__raw_writel(virt_to_phys(s3c_cpu_resume), REG_DIRECTGO_ADDR);
+	__raw_writel(EXYNOS_CHECK_DIRECTGO, REG_DIRECTGO_FLAG);
+
+	cpu_offset = cpuid ^ 0x4;
+
+	set_boot_flag(cpuid, C2_STATE);
+	cpu_pm_enter();
+
+	 __raw_writel(0x0, EXYNOS_ARM_CORE_CONFIGURATION(cpu_offset));
+
+	value = __raw_readl(EXYNOS5_ARM_INTR_SPREAD_ENABLE);
+	value &= ~(0x1 << cpu_offset);
+	__raw_writel(value, EXYNOS5_ARM_INTR_SPREAD_ENABLE);
+
+	ret = cpu_suspend(0, c2_finisher);
+	if (ret)
+		__raw_writel(0x3, EXYNOS_ARM_CORE_CONFIGURATION(cpu_offset));
+
+	value = __raw_readl(EXYNOS5_ARM_INTR_SPREAD_ENABLE);
+	value |= (0x1 << cpu_offset);
+	__raw_writel(value, EXYNOS5_ARM_INTR_SPREAD_ENABLE);
+
+	clear_boot_flag(cpuid, C2_STATE);
+	cpu_pm_exit();
+
+	do_gettimeofday(&after);
+	local_irq_enable();
+	idle_time = (after.tv_sec - before.tv_sec) * USEC_PER_SEC +
+		    (after.tv_usec - before.tv_usec);
+
+	dev->last_residency = idle_time;
+
+	return index;
+}
+#endif
+
+static int exynos_cpuidle_notifier_event(struct notifier_block *this,
+					  unsigned long event,
+					  void *ptr)
+{
+	switch (event) {
+	case PM_SUSPEND_PREPARE:
+		pr_debug("PM_SUSPEND_PREPARE for CPUIDLE\n");
+		return NOTIFY_OK;
+	case PM_POST_RESTORE:
+	case PM_POST_SUSPEND:
+		pr_debug("PM_POST_SUSPEND for CPUIDLE\n");
+		return NOTIFY_OK;
+	}
+	return NOTIFY_DONE;
+}
+
+static struct notifier_block exynos_cpuidle_notifier = {
+	.notifier_call = exynos_cpuidle_notifier_event,
+};
+
+static int __init exynos_init_cpuidle(void)
+{
+	int i, max_cpuidle_state, cpu_id;
+	struct cpuidle_device *device;
+	struct cpuidle_driver *drv = &exynos_idle_driver;
+	struct cpuidle_state *idle_set;
+
+	/* Setup cpuidle driver */
+	idle_set = exynos5_cpuidle_set;
+	drv->state_count = ARRAY_SIZE(exynos5_cpuidle_set);
+
+	max_cpuidle_state = drv->state_count;
+
+	for (i = 0; i < max_cpuidle_state; i++) {
+		memcpy(&drv->states[i], &idle_set[i],
+				sizeof(struct cpuidle_state));
+	}
+	drv->safe_state_index = 0;
+	cpuidle_register_driver(&exynos_idle_driver);
+
+	for_each_cpu(cpu_id, cpu_online_mask) {
+		device = &per_cpu(exynos_cpuidle_device, cpu_id);
+		device->cpu = cpu_id;
+
+		device->state_count = max_cpuidle_state;
+
+		if (cpuidle_register_device(device)) {
+			printk(KERN_ERR "CPUidle register device failed\n,");
+			return -EIO;
+		}
+	}
+
+	register_pm_notifier(&exynos_cpuidle_notifier);
+
+	return 0;
+}
+device_initcall(exynos_init_cpuidle);
