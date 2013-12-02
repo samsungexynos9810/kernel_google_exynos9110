c006a75c60ea1fd03c71b620417c21c7817e08db
Teahyung Kim <tkon.kim@samsung.com>
2013-12-03 02:42:12 +0900

    [COMMON] media: exynos: fimc-is: added new sensor module driver 4H5
    Change-Id: I8f5fe6e8fd89d1661f31baaa41c7289b56250b6d
    Signed-off-by: Teahyung Kim <tkon.kim@samsung.com>

---
+++ drivers/media/platform/exynos/fimc-is/sensor/fimc-is-device-4h5.c
@@ -0,0 +1,194 @@
+/*
+ * Samsung Exynos5 SoC series Sensor driver
+ *
+ *
+ * Copyright (c) 2011 Samsung Electronics Co., Ltd
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/i2c.h>
+#include <linux/slab.h>
+#include <linux/irq.h>
+#include <linux/interrupt.h>
+#include <linux/delay.h>
+#include <linux/version.h>
+#include <linux/gpio.h>
+#include <linux/clk.h>
+#include <linux/regulator/consumer.h>
+#include <linux/videodev2.h>
+#include <linux/videodev2_exynos_camera.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/platform_device.h>
+#include <mach/regs-gpio.h>
+#include <mach/regs-clock.h>
+#include <plat/clock.h>
+#include <plat/gpio-cfg.h>
+#include <media/v4l2-ctrls.h>
+#include <media/v4l2-device.h>
+#include <media/v4l2-subdev.h>
+#include <mach/exynos-fimc-is-sensor.h>
+
+#include "../fimc-is-core.h"
+#include "../fimc-is-device-sensor.h"
+#include "fimc-is-device-4h5.h"
+
+#define SENSOR_NAME "S5K4H5"
+
+static struct fimc_is_settle settle_4h5[] = {
+	/* 1936x1090@24fps */
+	FIMC_IS_SETTLE(1936, 1090, 24, 7),
+	/* 1936x1090@30fps */
+	FIMC_IS_SETTLE(1936, 1090, 30, 5),
+	/* 1936x1450@24fps */
+	FIMC_IS_SETTLE(1936, 1450, 24, 9),
+	/* 2064x1162@24fps */
+	FIMC_IS_SETTLE(2064, 1162, 24, 8),
+	/* 1936x1090@60fps */
+	FIMC_IS_SETTLE(1936, 1090, 60, 9),
+	/* 816x460@60fps */
+	FIMC_IS_SETTLE(816, 460, 60, 9),
+	/* 736x490@120fps */
+	FIMC_IS_SETTLE(736, 490, 120, 11),
+	/* 1296x730@120fps */
+	FIMC_IS_SETTLE(1296, 730, 120, 11),
+	/* 4112x2314@24ps */
+	FIMC_IS_SETTLE(4112, 2314, 24, 14),
+	/* 816x460@120fps */
+	FIMC_IS_SETTLE(816, 460, 120, 18),
+	/* 4144x2332@24fps */
+	FIMC_IS_SETTLE(4144, 2332, 24, 19),
+	/* 4144x2332@30fps */
+	FIMC_IS_SETTLE(4144, 2332, 30, 18),
+	/* 4112x3082@24fps */
+	FIMC_IS_SETTLE(4112, 3082, 24, 19),
+	/* 4144x3106@24fps */
+	FIMC_IS_SETTLE(4144, 3106, 24, 23),
+	/* 4144x3106@30fps */
+	FIMC_IS_SETTLE(4144, 3106, 30, 23),
+	/* 2048x1152@60fps */
+	FIMC_IS_SETTLE(2048, 1152, 60, 9),
+	/* 1024x576@120fps */
+	FIMC_IS_SETTLE(1024, 576, 120, 9),
+	/* 1024x576@60fps */
+	FIMC_IS_SETTLE(1024, 576, 60, 9),
+	/* 1936x1090@15fps */
+	FIMC_IS_SETTLE(1936, 1090, 15, 7),
+};
+
+static int sensor_4h5_init(struct v4l2_subdev *subdev, u32 val)
+{
+	int ret = 0;
+	struct fimc_is_module_enum *module;
+
+	BUG_ON(!subdev);
+
+	module = (struct fimc_is_module_enum *)v4l2_get_subdevdata(subdev);
+
+	pr_info("[MOD:D:%d] %s(%d)\n", module->id, __func__, val);
+
+	return ret;
+}
+
+static const struct v4l2_subdev_core_ops core_ops = {
+	.init = sensor_4h5_init
+};
+
+static const struct v4l2_subdev_ops subdev_ops = {
+	.core = &core_ops
+};
+
+int sensor_4h5_probe(struct i2c_client *client,
+	const struct i2c_device_id *id)
+{
+	int ret = 0;
+	struct fimc_is_core *core;
+	struct v4l2_subdev *subdev_module;
+	struct fimc_is_module_enum *module;
+	struct fimc_is_device_sensor *device;
+	struct sensor_open_extended *ext;
+
+	BUG_ON(!fimc_is_dev);
+
+	core = (struct fimc_is_core *)dev_get_drvdata(fimc_is_dev);
+	if (!core) {
+		err("core device is not yet probed");
+		return -EPROBE_DEFER;
+	}
+
+	device = &core->sensor[SENSOR_S5K4H5_INSTANCE];
+
+	subdev_module = kzalloc(sizeof(struct v4l2_subdev), GFP_KERNEL);
+	if (!subdev_module) {
+		err("subdev_module is NULL");
+		ret = -ENOMEM;
+		goto p_err;
+	}
+
+	module = &device->module_enum[atomic_read(&core->resourcemgr.rsccount_module)];
+	atomic_inc(&core->resourcemgr.rsccount_module);
+	//module = &device->module_enum[SENSOR_S5K4H5_NAME];
+	module->id = SENSOR_NAME_S5K4H5;
+	module->subdev = subdev_module;
+	module->device = SENSOR_S5K4H5_INSTANCE;
+	module->ops = NULL;
+	module->client = client;
+	module->pixel_width = 3264 + 16;
+	module->pixel_height = 2448 + 10;
+	module->active_width = 3264;
+	module->active_height = 2448;
+	module->max_framerate = 120;
+	module->setfile_name = "setfile_4h5.bin";
+	module->settle_max = ARRAY_SIZE(settle_4h5);
+	module->settle_table = settle_4h5;
+	module->ops = NULL;
+	module->private_data = NULL;
+
+	ext = &module->ext;
+	ext->I2CSclk = I2C_L0;
+
+	ext->sensor_con.product_name = 0;
+	ext->sensor_con.peri_type = SE_I2C;
+	ext->sensor_con.peri_setting.i2c.channel = SENSOR_CONTROL_I2C0;
+	ext->sensor_con.peri_setting.i2c.slave_address = 0x6E;
+	ext->sensor_con.peri_setting.i2c.speed = 400000;
+
+	ext->actuator_con.product_name = ACTUATOR_NAME_DW9804;
+	//ext->actuator_con.product_name = ACTUATOR_NAME_NOTHING;
+	ext->actuator_con.peri_type = SE_I2C;
+	ext->actuator_con.peri_setting.i2c.channel
+		= SENSOR_CONTROL_I2C0;
+
+	ext->flash_con.product_name = FLADRV_NAME_RT5033;
+	//ext->flash_con.product_name = FLADRV_NAME_NOTHING;
+	ext->flash_con.peri_type = SE_GPIO;
+	ext->flash_con.peri_setting.gpio.first_gpio_port_no = 8;
+	ext->flash_con.peri_setting.gpio.second_gpio_port_no = 6;
+
+	/* ext->from_con.product_name = FROMDRV_NAME_W25Q80BW; */
+	ext->from_con.product_name = FROMDRV_NAME_NOTHING;
+	ext->mclk = 0;
+	ext->mipi_lane_num = 0;
+	ext->mipi_speed = 0;
+	ext->fast_open_sensor = 0;
+	ext->self_calibration_mode = 0;
+	ext->I2CSclk = I2C_L0;
+
+	ext->companion_con.product_name = COMPANION_NAME_NOTHING;
+
+#ifdef DEFAULT_S5K4H5_DRIVING
+	v4l2_i2c_subdev_init(subdev_module, client, &subdev_ops);
+#else
+	v4l2_subdev_init(subdev_module, &subdev_ops);
+#endif
+	v4l2_set_subdevdata(subdev_module, module);
+	v4l2_set_subdev_hostdata(subdev_module, device);
+	snprintf(subdev_module->name, V4L2_SUBDEV_NAME_SIZE, "sensor-subdev.%d", module->id);
+
+p_err:
+	info("%s(%d)\n", __func__, ret);
+	return ret;
+}
