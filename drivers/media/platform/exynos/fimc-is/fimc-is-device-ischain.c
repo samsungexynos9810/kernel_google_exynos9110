67aa4035aaff9f0af2fd18cbeafd23c18d39f084
Jeongtae Park <jtp.park@samsung.com>
2013-12-06 17:43:10 +0900

    [COMMON] media: fimc-is: change DRC bypass to support 1.6.1
    Change-Id: I312a5b1dd9b88d8871c1862c86281493383a6854
    Signed-off-by: Jeongtae Park <jtp.park@samsung.com>

--- drivers/media/platform/exynos/fimc-is/fimc-is-device-ischain.c
+++ drivers/media/platform/exynos/fimc-is/fimc-is-device-ischain.c
@@ -3812,7 +3812,11 @@ static int fimc_is_ischain_drc_bypass(struct fimc_is_device_ischain *device,
 		goto p_err;
 	}
 
-	group_id |= GROUP_ID(group->id);
+	if (GET_FIMC_IS_NUM_OF_SUBIP2(device, 3a0) == 0 &&
+		GET_FIMC_IS_NUM_OF_SUBIP2(device, 3a1) == 0)
+		group_id = GROUP_ID(GROUP_ID_3A0);
+	else
+		group_id = GROUP_ID(group->id);
 
 	lindex = hindex = indexes = 0;
 	ctl_param = fimc_is_itf_g_param(device, frame, PARAM_DRC_CONTROL);
