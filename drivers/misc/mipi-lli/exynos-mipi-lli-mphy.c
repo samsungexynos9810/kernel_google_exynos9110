abb9a49467d52d3aceebd6bd7cd17b5b1c477275
Byeongjo Park <bjo.park@samsung.com>
2013-12-11 15:52:55 +0900

    [5430] misc: mipi-lli: set M-PHY's setting for sync with CP
    Update M-PHY settings for sync with CP
    
    Change-Id: I884a2cdab2f1d922c041713b45d0ce620917f562
    Signed-off-by: Byeongjo Park <bjo.park@samsung.com>

--- drivers/misc/mipi-lli/exynos-mipi-lli-mphy.c
+++ drivers/misc/mipi-lli/exynos-mipi-lli-mphy.c
@@ -59,7 +59,6 @@ int exynos_mphy_ovtm_init(struct exynos_mphy *phy)
 	writel(0x1A, phy->loc_regs + (0x40*4));
 
 	/* Reset-On-Error REQ timing configuration */
-	writel(0x30, phy->loc_regs + (0x5c*4));
 	writel(0x1D, phy->loc_regs + (0x77*4));
 
 	return 0;
