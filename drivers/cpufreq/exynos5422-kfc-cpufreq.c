ed8625735697308c84b4509c2b40335edeb5f4e5
Hyukjun Kweon <hjun.kweon@samsung.com>
2013-12-23 10:31:05 +0900

    Revert "[HACK:5422] cpufreq: exynos: added cpu max lock after resume"
    This reverts commit 38d5909cc576134bfcb5240c08a5c9e30497988d.

--- drivers/cpufreq/exynos5422-kfc-cpufreq.c
+++ drivers/cpufreq/exynos5422-kfc-cpufreq.c
@@ -15,7 +15,6 @@
 #include <linux/io.h>
 #include <linux/slab.h>
 #include <linux/cpufreq.h>
-#include <linux/suspend.h>
 #include <linux/clk-private.h>
 #include <linux/pm_qos.h>
 
@@ -32,7 +31,6 @@
 
 static int max_support_idx_CA7;
 static int min_support_idx_CA7 = (CPUFREQ_LEVEL_END_CA7 - 1);
-static struct pm_qos_request resume_max_kfc_qos;
 
 static struct clk *fout_kpll;
 static struct clk *fout_spll;
@@ -461,26 +459,6 @@ static bool exynos5422_is_alive_CA7(void)
 	return tmp ? true : false;
 }
 
-#ifdef CONFIG_PM
-static int exynos_cpufreq_kfc_pm_notifier(struct notifier_block *notifier,
-		unsigned long pm_event, void *v)
-{
-	switch (pm_event) {
-	case PM_SUSPEND_PREPARE:
-		break;
-	case PM_POST_SUSPEND:
-		pm_qos_update_request_timeout(&resume_max_kfc_qos, 1000 * 1000, 5000 * 1000);
-		break;
-	}
-	return NOTIFY_OK;
-}
-
-static struct notifier_block exynos_cpufreq_kfc_nb = {
-	.notifier_call = exynos_cpufreq_kfc_pm_notifier,
-	.priority = 1,
-};
-#endif
-
 int __init exynos5_cpufreq_CA7_init(struct exynos_dvfs_info *info)
 {
 	int i;
@@ -564,13 +542,6 @@ int __init exynos5_cpufreq_CA7_init(struct exynos_dvfs_info *info)
 	info->set_freq = exynos5422_set_frequency_CA7;
 	info->need_apll_change = exynos5422_pms_change_CA7;
 	info->is_alive = exynos5422_is_alive_CA7;
-
-#ifdef CONFIG_PM
-	register_pm_notifier(&exynos_cpufreq_kfc_nb);
-
-	pm_qos_add_request(&resume_max_kfc_qos, PM_QOS_KFC_FREQ_MAX,
-			PM_QOS_KFC_FREQ_MAX_DEFAULT_VALUE);
-#endif
 
 #ifdef ENABLE_CLKOUT
 	tmp = __raw_readl(EXYNOS5_CLKOUT_CMU_KFC);
