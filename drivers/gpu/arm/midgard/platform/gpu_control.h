c5d9f58bba02949dee54821694cc4da91ca2985f
Kim Hyuncheol <host.kim@samsung.com>
2014-01-04 19:03:10 +0900

    [5422] gpu: midgard: Enable the 543 Mhz as one of the GPU clocks
    Change-Id: I30623db3973de83dad79f84802588ac160fc2ec4
    Signed-off-by: Kim Hyuncheol <host.kim@samsung.com>

--- drivers/gpu/arm/midgard/platform/gpu_control.h
+++ drivers/gpu/arm/midgard/platform/gpu_control.h
@@ -36,7 +36,11 @@ typedef enum {
 
 /* mali_kbase_platform */
 #if SOC_NAME == 5422
-#define G3D_MAX_FREQ    420000
+#ifdef CONFIG_SOC_EXYNOS5422_REV_0
+#define G3D_MAX_FREQ    543000
+#else
+#define G3D_MAX_FREQ    533000
+#endif /* CONFIG_SOC_EXYNOS5422_REV_0 */
 #define G3D_MIN_FREQ    177000
 #elif SOC_NAME == 5430
 #define G3D_MAX_FREQ    600000
