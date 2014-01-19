49868c95a53eb54e765ba085c1d21b78bd3226b8
Cho KyongHo <pullip.cho@samsung.com>
2014-01-19 20:16:06 +0900

    [COMMON] gpu: ion: fix ion_buffer_destroy() call in spin_lock
    This patch is a follow-up of "ion: Avoid using rt_mutexes directly".
    that is not correctly applied.
    
    Change-Id: I9378f1fad39c8c5c5f30364ab553b737193486fd
    Signed-off-by: Cho KyongHo <pullip.cho@samsung.com>

--- drivers/gpu/ion/ion_heap.c
+++ drivers/gpu/ion/ion_heap.c
@@ -190,7 +190,7 @@ size_t ion_heap_freelist_size(struct ion_heap *heap)
 
 size_t ion_heap_freelist_drain(struct ion_heap *heap, size_t size)
 {
-	struct ion_buffer *buffer, *tmp;
+	struct ion_buffer *buffer;
 	size_t total_drained = 0;
 
 	if (ion_heap_freelist_size(heap) == 0)
@@ -200,13 +200,17 @@ size_t ion_heap_freelist_drain(struct ion_heap *heap, size_t size)
 	if (size == 0)
 		size = heap->free_list_size;
 
-	list_for_each_entry_safe(buffer, tmp, &heap->free_list, list) {
+	while (!list_empty(&heap->free_list)) {
 		if (total_drained >= size)
 			break;
+		buffer = list_first_entry(&heap->free_list, struct ion_buffer,
+					  list);
 		list_del(&buffer->list);
 		heap->free_list_size -= buffer->size;
 		total_drained += buffer->size;
+		spin_unlock(&heap->free_lock);
 		ion_buffer_destroy(buffer);
+		spin_lock(&heap->free_lock);
 	}
 	spin_unlock(&heap->free_lock);
 
