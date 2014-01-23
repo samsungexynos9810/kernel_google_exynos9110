4d1506a1e0ac42d82fa8ae48d04a56ef00fce83b
JaeHun Jung <jh0801.jung@samsung.com>
2014-01-23 17:42:20 +0900

    [COMMON] mmc: dw_mmc: Disable DWMCI_AXI_NON_BLOCKING_WRITE.
    Change-Id: Ieef408780088ef6975c2c75c325cda2f4536d564
    Signed-off-by: JaeHun Jung <jh0801.jung@samsung.com>

--- drivers/mmc/host/dw_mmc-exynos.c
+++ drivers/mmc/host/dw_mmc-exynos.c
@@ -396,7 +396,7 @@ static void dw_mci_exynos_set_ios(struct dw_mci *host, unsigned int tuning, stru
 		clksel = ((priv->ddr200_timing & 0xfffffff8) | pdata->clk_smpl);
 
 		if (!tuning) {
-			rddqs |= (DWMCI_RDDQS_EN | DWMCI_AXI_NON_BLOCKING_WRITE);
+			rddqs |= DWMCI_RDDQS_EN;
 			if (priv->delay_line)
 				dline = DWMCI_FIFO_CLK_DELAY_CTRL(0x2) |
 				DWMCI_RD_DQS_DELAY_CTRL(priv->delay_line);
