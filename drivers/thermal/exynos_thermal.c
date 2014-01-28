c579a4011d5a72dd19dc8baf8c8b332a61ce6318
Jongpill Lee <boyko.lee@samsung.com>
2014-01-28 17:54:27 +0900

    [5430] thermal: Apply DTM guide with IPA feature
    Change-Id: I2458d9953c338c9815883ef73d8802c5136d61d3
    Signed-off-by: Jongpill Lee <boyko.lee@samsung.com>

--- drivers/thermal/exynos_thermal.c
+++ drivers/thermal/exynos_thermal.c
@@ -214,7 +214,7 @@ static struct cpumask mp_cluster_cpus[CA_END];
 
 #if defined(CONFIG_SOC_EXYNOS5430)
 #define CPU_HOTPLUG_IN_TEMP	95
-#define CPU_HOTPLUG_OUT_TEMP	110
+#define CPU_HOTPLUG_OUT_TEMP	100
 #elif defined(CONFIG_SOC_EXYNOS5422)
 #define CPU_HOTPLUG_IN_TEMP	95
 #define CPU_HOTPLUG_OUT_TEMP	100
@@ -1508,14 +1508,14 @@ static struct exynos_tmu_platform_data const exynos5430_evt0_tmu_data = {
 #if defined(CONFIG_SOC_EXYNOS5430_REV_1)
 static struct exynos_tmu_platform_data const exynos5430_tmu_data = {
 	.threshold_falling = 2,
-	.trigger_levels[0] = 80,
-	.trigger_levels[1] = 90,
-	.trigger_levels[2] = 100,
-	.trigger_levels[3] = 110,
-	.trigger_levels[4] = 110,
-	.trigger_levels[5] = 110,
-	.trigger_levels[6] = 110,
-	.trigger_levels[7] = 115,
+	.trigger_levels[0] = 95,
+	.trigger_levels[1] = 100,
+	.trigger_levels[2] = 105,
+	.trigger_levels[3] = 105,
+	.trigger_levels[4] = 105,
+	.trigger_levels[5] = 105,
+	.trigger_levels[6] = 105,
+	.trigger_levels[7] = 110,
 	.trigger_level0_en = 1,
 	.trigger_level1_en = 1,
 	.trigger_level2_en = 1,
@@ -1530,22 +1530,22 @@ static struct exynos_tmu_platform_data const exynos5430_tmu_data = {
 	.cal_type = TYPE_ONE_POINT_TRIMMING,
 	.efuse_value = 75,
 	.freq_tab[0] = {
-		.freq_clip_max = 1300 * 1000,
+		.freq_clip_max = 900 * 1000,
 #ifdef CONFIG_ARM_EXYNOS_MP_CPUFREQ
 		.freq_clip_max_kfc = 1500 * 1000,
 #endif
-		.temp_level = 80,
+		.temp_level = 95,
 #ifdef CONFIG_ARM_EXYNOS_MP_CPUFREQ
 		.mask_val = &mp_cluster_cpus[CA15],
 		.mask_val_kfc = &mp_cluster_cpus[CA7],
 #endif
 	},
 	.freq_tab[1] = {
-		.freq_clip_max = 1100 * 1000,
+		.freq_clip_max = 800 * 1000,
 #ifdef CONFIG_ARM_EXYNOS_MP_CPUFREQ
-		.freq_clip_max_kfc = 1500 * 1000,
+		.freq_clip_max_kfc = 800 * 1000,
 #endif
-		.temp_level = 90,
+		.temp_level = 100,
 #ifdef CONFIG_ARM_EXYNOS_MP_CPUFREQ
 		.mask_val = &mp_cluster_cpus[CA15],
 		.mask_val_kfc = &mp_cluster_cpus[CA7],
@@ -1554,9 +1554,9 @@ static struct exynos_tmu_platform_data const exynos5430_tmu_data = {
 	.freq_tab[2] = {
 		.freq_clip_max = 800 * 1000,
 #ifdef CONFIG_ARM_EXYNOS_MP_CPUFREQ
-		.freq_clip_max_kfc = 1500 * 1000,
+		.freq_clip_max_kfc = 800 * 1000,
 #endif
-		.temp_level = 100,
+		.temp_level = 105,
 #ifdef CONFIG_ARM_EXYNOS_MP_CPUFREQ
 		.mask_val = &mp_cluster_cpus[CA15],
 		.mask_val_kfc = &mp_cluster_cpus[CA7],
@@ -1567,7 +1567,7 @@ static struct exynos_tmu_platform_data const exynos5430_tmu_data = {
 #ifdef CONFIG_ARM_EXYNOS_MP_CPUFREQ
 		.freq_clip_max_kfc = 800 * 1000,
 #endif
-		.temp_level = 110,
+		.temp_level = 105,
 #ifdef CONFIG_ARM_EXYNOS_MP_CPUFREQ
 		.mask_val = &mp_cluster_cpus[CA15],
 		.mask_val_kfc = &mp_cluster_cpus[CA7],
@@ -1578,7 +1578,7 @@ static struct exynos_tmu_platform_data const exynos5430_tmu_data = {
 #ifdef CONFIG_ARM_EXYNOS_MP_CPUFREQ
 		.freq_clip_max_kfc = 800 * 1000,
 #endif
-		.temp_level = 110,
+		.temp_level = 105,
 #ifdef CONFIG_ARM_EXYNOS_MP_CPUFREQ
 		.mask_val = &mp_cluster_cpus[CA15],
 		.mask_val_kfc = &mp_cluster_cpus[CA7],
@@ -1589,7 +1589,7 @@ static struct exynos_tmu_platform_data const exynos5430_tmu_data = {
 #ifdef CONFIG_ARM_EXYNOS_MP_CPUFREQ
 		.freq_clip_max_kfc = 800 * 1000,
 #endif
-		.temp_level = 110,
+		.temp_level = 105,
 #ifdef CONFIG_ARM_EXYNOS_MP_CPUFREQ
 		.mask_val = &mp_cluster_cpus[CA15],
 		.mask_val_kfc = &mp_cluster_cpus[CA7],
@@ -1600,7 +1600,7 @@ static struct exynos_tmu_platform_data const exynos5430_tmu_data = {
 #ifdef CONFIG_ARM_EXYNOS_MP_CPUFREQ
 		.freq_clip_max_kfc = 800 * 1000,
 #endif
-		.temp_level = 110,
+		.temp_level = 105,
 #ifdef CONFIG_ARM_EXYNOS_MP_CPUFREQ
 		.mask_val = &mp_cluster_cpus[CA15],
 		.mask_val_kfc = &mp_cluster_cpus[CA7],
