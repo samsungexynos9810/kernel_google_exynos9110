d9d103dde4b44e833f9be2a23005da54444359b4
Taekki Kim <taekki.kim@samsung.com>
2014-02-07 17:29:08 +0900

    [5422] cpufreq: exynos: add KFC boosting frequency
    This patch adds KFC boosting frequency to 1.0GHz when
    EGL minimum PM QoS is set.
    
    Change-Id: I49009264a930b661f15738243329211829757778
    Signed-off-by: Taekki Kim <taekki.kim@samsung.com>

--- drivers/cpufreq/exynos5422-kfc-cpufreq.c
+++ drivers/cpufreq/exynos5422-kfc-cpufreq.c
@@ -496,6 +496,7 @@ int __init exynos5_cpufreq_CA7_init(struct exynos_dvfs_info *info)
 	info->pll_safe_idx = L5;
 	info->max_support_idx = max_support_idx_CA7;
 	info->min_support_idx = min_support_idx_CA7;
+	info->boost_freq = exynos5422_freq_table_CA7[L6].frequency;
 	info->boot_cpu_min_qos = exynos5422_freq_table_CA7[L2].frequency;
 	info->boot_cpu_max_qos = exynos5422_freq_table_CA7[L2].frequency;
 	info->cpu_clk = fout_kpll;
