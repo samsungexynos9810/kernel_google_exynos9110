27a05b68bb19cbc085771dd78b7ec6616defce24
Hwang seungkon <skon.hwang@samsung.com>
2014-02-22 00:35:18 +0900

    [COMMON] sched: hmp: Child inherits sched_avg memeber from parent when executed fork()
    For preventing up & down migration frequently, do not initialize to zero
    about runnable_avg_sum, runnable_avg_period.
    Instead, these memebers are initialized to zero only one by idle task.
    
    Change-Id: I810201e95e456504fff74099800d7253378301ef
    Signed-off-by: Hwang seungkon <skon.hwang@samsung.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1616,12 +1616,13 @@ static void __sched_fork(struct task_struct *p)
  * load-balance).
  */
 #if defined(CONFIG_SMP) && defined(CONFIG_FAIR_GROUP_SCHED)
-	p->se.avg.runnable_avg_period = 0;
-	p->se.avg.runnable_avg_sum = 0;
 	p->se.avg.remainder = 0;
 #ifdef CONFIG_SCHED_HMP
 	p->se.avg.hmp_last_up_migration = 0;
 	p->se.avg.hmp_last_down_migration = 0;
+#else
+	p->se.avg.runnable_avg_period = 0;
+	p->se.avg.runnable_avg_sum = 0;
 #endif
 #endif
 #ifdef CONFIG_SCHEDSTATS
@@ -4747,6 +4748,10 @@ void __cpuinit init_idle(struct task_struct *idle, int cpu)
 	raw_spin_lock_irqsave(&rq->lock, flags);
 
 	__sched_fork(idle);
+#ifdef CONFIG_SCHED_HMP
+	idle->se.avg.runnable_avg_period = 0;
+	idle->se.avg.runnable_avg_sum = 0;
+#endif
 	idle->state = TASK_RUNNING;
 	idle->se.exec_start = sched_clock();
 
