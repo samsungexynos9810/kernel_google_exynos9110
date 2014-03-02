b898d9fe0af11ec2b6d482d438f3dfb2151c88fa
Wooki Min <wooki.min@samsung.com>
2014-03-02 22:44:32 +0900

    [COMMON] ARM: S5P: Add FIMC_FD VA address mapping
    Change-Id: Id8cce1db73131757d0fdb5650d1721dbfc51e9f8
    Signed-off-by: Wooki Min <wooki.min@samsung.com>

--- arch/arm/plat-samsung/include/plat/map-s5p.h
+++ arch/arm/plat-samsung/include/plat/map-s5p.h
@@ -85,6 +85,8 @@
 
 #define S5P_VA_SYSREG		S3C_ADDR(0x02BA0000)
 
+#define S5P_VA_FIMC_FD		S3C_ADDR(0x02BB0000)
+
 #define VA_VIC(x)		(S3C_VA_IRQ + ((x) * 0x10000))
 #define VA_VIC0			VA_VIC(0)
 #define VA_VIC1			VA_VIC(1)
