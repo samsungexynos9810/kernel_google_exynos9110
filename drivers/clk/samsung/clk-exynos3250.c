da9c3c9cbebd1d506f319837de04e4df04c07c9d
Kisang Lee <kisang80.lee@samsung.com>
2014-03-06 22:29:43 +0900

    [3250] clk: exynos: Added UART clock support
    This patch adds UART clock support for exynos3250 board.
    
    Change-Id: I0db6859c367a5ea4af91803d62194b1f3c378dfc
    Signed-off-by: Kisang Lee <kisang80.lee@samsung.com>

--- drivers/clk/samsung/clk-exynos3250.c
+++ drivers/clk/samsung/clk-exynos3250.c
@@ -29,7 +29,11 @@ enum exynos3250_clks {
 	dclk_gpr = 60, dclk_gdr,
 
 	clk_mct = 80,
+	mout_uart0, mout_uart1, mout_uart2, mout_uart3,
+	sclk_uart0 = 200 , sclk_uart1, sclk_uart2, sclk_uart3,
+	dout_uart0 = 250, dout_uart1, dout_uart2, dout_uart3,
 
+	gate_uart0 = 300, gate_uart1, gate_uart2, gate_uart3,
 	nr_clks,
 };
 
@@ -39,21 +43,73 @@ static __initdata void *exynos3250_clk_regs[] = {
 	EXYNOS3_CLKSRC_RIGHTBUS,
 };
 
+#define CGATE_A(_id, cname, pname, o, b, f, gf, a) \
+	GATE_A(_id, cname, pname, (unsigned long)o, b, f, gf, a)
+
+#define CGATE(_id, cname, pname, o, b, f, gf) \
+	GATE(_id, cname, pname, (unsigned long)o, b, f, gf)
+
 struct samsung_gate_clock exynos3250_gate_clks[] __initdata = {
-	GATE_A(clk_mct, "clk_mct", "dclk_gpr", (unsigned long) EXYNOS3_CLKGATE_IP_PERIR, 13, CLK_IGNORE_UNUSED, 0, "mct"),
+	GATE_A(clk_mct, "clk_mct", "dclk_gpr", (unsigned long) EXYNOS3_CLKGATE_IP_PERIR,
+			13, CLK_IGNORE_UNUSED, 0, "mct"),
+	CGATE(sclk_uart0, "sclk_uart0", "dout_uart0",
+			EXYNOS3_CLKSRC_MASK_PERIL0, 0, CLK_SET_RATE_PARENT, 0),
+	CGATE(sclk_uart1, "sclk_uart1", "dout_uart1",
+			EXYNOS3_CLKSRC_MASK_PERIL0, 4, CLK_IGNORE_UNUSED, 0),
+	CGATE(sclk_uart1, "sclk_uart2", "dout_uart2",
+			EXYNOS3_CLKSRC_MASK_PERIL0, 8, CLK_SET_RATE_PARENT, 0),
+	CGATE(sclk_uart1, "sclk_uart3", "dout_uart3",
+			EXYNOS3_CLKSRC_MASK_PERIL0, 12, CLK_SET_RATE_PARENT, 0),
+
+	CGATE(gate_uart0, "gate_uart0", NULL,
+			EXYNOS3_CLKGATE_IP_PERIL, 0, CLK_IGNORE_UNUSED, 0),
+	CGATE(gate_uart1, "gate_uart1", NULL,
+			EXYNOS3_CLKGATE_IP_PERIL, 1, CLK_IGNORE_UNUSED, 0),
+	CGATE(gate_uart2, "gate_uart2", NULL,
+			EXYNOS3_CLKGATE_IP_PERIL, 2, CLK_IGNORE_UNUSED, 0),
+	CGATE(gate_uart3, "gate_uart3", NULL,
+			EXYNOS3_CLKGATE_IP_PERIL, 3, CLK_IGNORE_UNUSED, 0),
+};
+
+#define CDIV(_id, cname, pname, o, s, w) \
+			DIV(_id, cname, pname, (unsigned long)o, s, w)
+
+struct samsung_div_clock exynos3250_div_clks[] __initdata = {
+	DIV_A(dclk_gpr, "dclk_gpr", "dclk_gdr", (unsigned long) EXYNOS3_CLKDIV_RIGHTBUS,
+			4, 3, "div_gpr"),
+	DIV_A(dclk_gdr, "dclk_gdr", "mux_gdr", (unsigned long) EXYNOS3_CLKDIV_RIGHTBUS,
+			0, 4, "div_gdr"),
+
+	CDIV(dout_uart0, "dout_uart0", "mout_uart0",
+				EXYNOS3_CLKDIV_PERIL0, 0, 4),
+	CDIV(dout_uart1, "dout_uart1", "mout_uart1",
+				EXYNOS3_CLKDIV_PERIL0, 4, 4),
+	CDIV(dout_uart2, "dout_uart2", "mout_uart2",
+				EXYNOS3_CLKDIV_PERIL0, 8, 4),
+	CDIV(dout_uart3, "dout_uart3", "mout_uart3",
+				EXYNOS3_CLKDIV_PERIL0, 12, 4),
 };
 
 PNAME(mout_dclk_gdr)	= { "mout_user_bus_r" };
-PNAME(mout_ctrl_user_bus_r)	= { "fin_pll" };
+PNAME(mout_ctrl_user_bus_r)	= { "finpll" };
+PNAME(mout_uart_p)      = { "xxti", "xusbsti", "none", "none", "none",
+				"sclk_epll", "sclk_vpll" };
 
-struct samsung_div_clock exynos3250_div_clks[] __initdata = {
-	DIV_A(dclk_gpr, "dclk_gpr", "dclk_gdr", (unsigned long) EXYNOS3_CLKDIV_RIGHTBUS, 4, 3, "div_gpr"),
-	DIV_A(dclk_gdr, "mux_gdr", "dclk_gdr", (unsigned long) EXYNOS3_CLKDIV_RIGHTBUS, 0, 4, "div_gdr"),
-};
+#define CMUX(_id, cname, pnames, o, s, w) \
+	MUX(_id, cname, pnames, (unsigned long)o, s, w)
 
 struct samsung_mux_clock exynos3250_mux_clks[] __initdata = {
 	MUX(mux_gdr, "mux_gdr", mout_dclk_gdr, (unsigned long) EXYNOS3_CLKSRC_RIGHTBUS, 0, 1),
 	MUX(mout_user_bus_r, "mout_user_bus_r", mout_ctrl_user_bus_r, (unsigned long) EXYNOS3_CLKSRC_RIGHTBUS, 4, 1),
+
+	CMUX(mout_uart0, "mout_uart0", mout_uart_p,
+			EXYNOS3_CLKSRC_PERIL0, 0, 4),
+	CMUX(mout_uart1, "mout_uart1", mout_uart_p,
+			EXYNOS3_CLKSRC_PERIL0, 4, 4),
+	CMUX(mout_uart2, "mout_uart2", mout_uart_p,
+			EXYNOS3_CLKSRC_PERIL0, 8, 4),
+	CMUX(mout_uart3, "mout_uart3", mout_uart_p,
+			EXYNOS3_CLKSRC_PERIL0, 12, 4),
 };
 
 /* fixed rate clocks generated outside the soc */
