c5357204aa60bc21c921e9e528c68b6a8407959a
Lee Yongjin <yongjin0.lee@samsung.com>
2014-04-24 07:17:11 +0900

    [5433] ARM: exynos: Support the BTS interface for EXYNOS5433
    Change-Id: I2cbcdf7f6badd68fcb93cc7ee78bfa6e97290a79
    Signed-off-by: Lee Yongjin <yongjin0.lee@samsung.com>

--- arch/arm/mach-exynos/include/mach/bts.h
+++ arch/arm/mach-exynos/include/mach/bts.h
@@ -11,13 +11,14 @@
 #ifndef __EXYNOS_BTS_H_
 #define __EXYNOS_BTS_H_
 
-#if defined(CONFIG_EXYNOS5430_BTS) || defined(CONFIG_EXYNOS5422_BTS)
+#if defined(CONFIG_EXYNOS5430_BTS) || defined(CONFIG_EXYNOS5422_BTS)	\
+	|| defined(CONFIG_EXYNOS5433_BTS)
 void bts_initialize(const char *pd_name, bool on);
 #else
 #define bts_initialize(a, b) do {} while (0)
 #endif
 
-#if defined(CONFIG_EXYNOS5430_BTS)
+#if defined(CONFIG_EXYNOS5430_BTS) || defined(CONFIG_EXYNOS5433_BTS)
 void bts_otf_initialize(unsigned int id, bool on);
 #else
 #define bts_otf_initialize(a, b) do {} while (0)
