473fb192fdda7498d795feb94f3ebf878e2fa36b
Shirish <s.shirish@samsung.com>
2014-07-01 10:12:50 +0530

    [common]: video: fimd: update the DMA burst length
    This patch changes the DMA burst length to 16 word-burst
    from 8 word-burst.
    This is needed to improve the performance.
    
    Change-Id: Icbea6a0d3844f7aaab6bc958cc80647edf50bcb4
    Signed-off-by: Shirish <s.shirish@samsung.com>
    Reviewed-on: http://107.108.3.115:8081/4675
    Reviewed-by: ritesh solanki <r.solanki@samsung.com>

--- drivers/video/decon_display/fimd_fb.c
+++ drivers/video/decon_display/fimd_fb.c
@@ -452,8 +452,7 @@ static inline u32 wincon(u32 bits_per_pixel, u32 transp_length, u32 red_length)
 			data |= WINCON0_BPPMODE_24BPP_888;
 
 		data |= WINCONx_WSWP;
-
-		data |= WINCONx_BURSTLEN_8WORD;
+		data |= WINCONx_BURSTLEN_16WORD;
 		break;
 	}
 
