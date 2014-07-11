858a87dbdc8c39606c475e22d9adbeed71d99380
Ritesh Solanki <r.solanki@samsung.com>
2014-07-11 16:23:03 +0530

    Revert "[common]: video: fimd: update the DMA burst length"
    This reverts commit 473fb192fdda7498d795feb94f3ebf878e2fa36b.
    
    Change-Id: Ib47fb19acbec7976f3657367425cddb22010fc74
    Reviewed-on: http://107.108.3.115:8081/4807
    Reviewed-by: ritesh solanki <r.solanki@samsung.com>
    Tested-by: ritesh solanki <r.solanki@samsung.com>
    Reviewed-by: Pankaj Kumar Dubey <pankaj.dubey@samsung.com>

--- drivers/video/decon_display/fimd_fb.c
+++ drivers/video/decon_display/fimd_fb.c
@@ -452,7 +452,8 @@ static inline u32 wincon(u32 bits_per_pixel, u32 transp_length, u32 red_length)
 			data |= WINCON0_BPPMODE_24BPP_888;
 
 		data |= WINCONx_WSWP;
-		data |= WINCONx_BURSTLEN_16WORD;
+
+		data |= WINCONx_BURSTLEN_8WORD;
 		break;
 	}
 
