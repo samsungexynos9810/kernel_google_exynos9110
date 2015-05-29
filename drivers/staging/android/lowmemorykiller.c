0c4f6a56977f3313172d0aa456e07322c2fb5829
Jinsung Yang <jsgood.yang@samsung.com>
2015-05-29 09:58:30 +0900

    [COMMON] lowmemorykiller: avoid killing self and page allocating process
    LMK was selecting the process to kill and setting the flag "TIF_MEMDIE",
    even though it was trying to allocate pages, causing the page
    allocation to fail.
    New flag "TIF_MEMALLOC" is added to avoid self kill and the process
    allocating pages.
    
    Change-Id: I19aa57db6fd46be284d5e0069a17528eeebc4321
    Signed-off-by: Jinsung Yang <jsgood.yang@samsung.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -65,6 +65,22 @@ static unsigned long lowmem_deathpending_timeout;
 			pr_info(x);			\
 	} while (0)
 
+static int test_task_flag(struct task_struct *p, int flag)
+{
+	struct task_struct *t = p;
+
+	do {
+		task_lock(t);
+		if (test_tsk_thread_flag(t, flag)) {
+			task_unlock(t);
+			return 1;
+		}
+		task_unlock(t);
+	} while_each_thread(p, t);
+
+	return 0;
+}
+
 static unsigned long lowmem_count(struct shrinker *s,
 				  struct shrink_control *sc)
 {
@@ -123,6 +139,10 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 		if (tsk->flags & PF_KTHREAD)
 			continue;
 
+#if defined(CONFIG_ARM) || defined(CONFIG_ARM64)
+		if (test_task_flag(tsk, TIF_MEMALLOC))
+			continue;
+#endif
 		p = find_lock_task_mm(tsk);
 		if (!p)
 			continue;
@@ -141,6 +161,8 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 		tasksize = get_mm_rss(p->mm);
 		task_unlock(p);
 		if (tasksize <= 0)
+			continue;
+		if (same_thread_group(p, current))
 			continue;
 		if (selected) {
 			if (oom_score_adj < selected_oom_score_adj)
