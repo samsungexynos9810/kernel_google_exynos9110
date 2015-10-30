3d8727055e6529c18ecf27c24efb99666567dc1a
Cho KyongHo <pullip.cho@samsung.com>
2015-10-30 22:44:53 +0900

    [COMMON] android: sync: move the position of sync_fence.rmnode
    It is found that sync_fence.rmnode->next is corrupted by the invalid
    reference to of sync_fence.wq by the device drivers that calls
    sync_fence_wait() and so forth.
    
    To prevent the corruption, it is moved to the place that is never
    modified after the creation of sync_fence.
    
    Change-Id: Iffbb3ba2f5db7b6567d3419801e27eaccbca2e5d
    Signed-off-by: Cho KyongHo <pullip.cho@samsung.com>

--- drivers/staging/android/sync.h
+++ drivers/staging/android/sync.h
@@ -154,16 +154,16 @@ struct sync_fence_cb {
 struct sync_fence {
 	struct file		*file;
 	struct kref		kref;
-	char			name[32];
+	union {
+		char			name[32];
+		struct llist_node	rmnode;
+	};
 #ifdef CONFIG_DEBUG_FS
 	struct list_head	sync_fence_list;
 #endif
 	int num_fences;
 
-	union {
-		wait_queue_head_t	wq;
-		struct llist_node	rmnode;
-	};
+	wait_queue_head_t	wq;
 	atomic_t		status;
 
 	struct sync_fence_cb	cbs[];
