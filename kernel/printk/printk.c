f718e270381fc7d9be833d3c26898ad29806d6a8
Hosung Kim <hosung0.kim@samsung.com>
2015-11-19 16:42:40 +0900

    printk: add output REAL core information
    This commit adds output real core information from MPIDR_el1
    registers. We used to know core information from smp_processor_id().
    Unfortunately, we don't trust core information when task struct is
    broken or task region is broken. Because it used task struct
    information. So we need REAL core information from MPIDR_el1
    registers.
    
    Change-Id: I8baa05a86f15bdfdc93d9223cccb38dfef91de35
    Signed-off-by: Hosung Kim <hosung0.kim@samsung.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -48,6 +48,7 @@
 #include <linux/uio.h>
 
 #include <asm/uaccess.h>
+#include <asm/cputype.h>
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/printk.h>
@@ -3244,12 +3245,22 @@ void __init dump_stack_set_arch_desc(const char *fmt, ...)
  */
 void dump_stack_print_info(const char *log_lvl)
 {
+#ifdef CONFIG_ARM64
+	printk("%sCPU: %d MPIDR: %llx PID: %d Comm: %.20s %s %s %.*s\n",
+	       log_lvl, raw_smp_processor_id(), read_cpuid_mpidr(),
+	       current->pid, current->comm,
+	       print_tainted(), init_utsname()->release,
+	       (int)strcspn(init_utsname()->version, " "),
+	       init_utsname()->version);
+
+#else
 	printk("%sCPU: %d PID: %d Comm: %.20s %s %s %.*s\n",
 	       log_lvl, raw_smp_processor_id(), current->pid, current->comm,
 	       print_tainted(), init_utsname()->release,
 	       (int)strcspn(init_utsname()->version, " "),
 	       init_utsname()->version);
 
+#endif
 	if (dump_stack_arch_desc_str[0] != '\0')
 		printk("%sHardware name: %s\n",
 		       log_lvl, dump_stack_arch_desc_str);
