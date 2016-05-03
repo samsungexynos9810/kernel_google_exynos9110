18370ab549335d6a1882681e443a20b9a0d07874
Hyunju Kang <hjtop.kang@samsung.com>
2016-05-03 11:13:40 +0900

    arm64: add ect_init_map_io for ECT
    Change-Id: I38a7bb75ef793fc9e9d7d9f10fd0e39ffd41530f
    Signed-off-by: Hyunju Kang <hjtop.kang@samsung.com>

--- arch/arm64/mm/mmu.c
+++ arch/arm64/mm/mmu.c
@@ -46,6 +46,12 @@
 
 #include "mm.h"
 
+#include <linux/vmalloc.h>
+
+#if defined(CONFIG_ECT)
+#include <soc/samsung/ect_parser.h>
+#endif
+
 u64 idmap_t0sz = TCR_T0SZ(VA_BITS);
 static int iotable_on;
 
@@ -548,6 +554,12 @@ void __init paging_init(void)
 {
 	phys_addr_t pgd_phys = early_pgtable_alloc();
 	pgd_t *pgd = pgd_set_fixmap(pgd_phys);
+
+	//map_mem();
+#if defined(CONFIG_ECT)
+	ect_init_map_io();
+#endif
+	//fixup_executable();
 
 	map_kernel(pgd);
 	map_mem(pgd);
