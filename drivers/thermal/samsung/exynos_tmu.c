9665fc4e9eccb3a6cc212b71315f9d0b3b850ca6
Jinkyu, Yang <jinkyu1.yang@samsung.com>
2016-10-18 18:04:19 +0900

    [COMMON] thermal: samsung: declare external variable for GPU sysfs
    Change-Id: Iebd1587701adb7f353ab1c57c8b5460167bf518b
    Signed-off-by: Jinkyu, Yang <jinkyu1.yang@samsung.com>

--- drivers/thermal/samsung/exynos_tmu.c
+++ drivers/thermal/samsung/exynos_tmu.c
@@ -1266,6 +1266,11 @@ static int exynos_cpufreq_cooling_register(struct exynos_tmu_data *data)
 }
 
 #ifdef CONFIG_GPU_THERMAL
+
+#ifdef CONFIG_MALI_DEBUG_KERNEL_SYSFS
+struct exynos_tmu_data *gpu_thermal_data = NULL;
+#endif
+
 static int exynos_gpufreq_cooling_register(struct exynos_tmu_data *data)
 {
 	struct device_node *np, *child = NULL, *gchild, *ggchild;
@@ -1321,6 +1326,10 @@ static int exynos_gpufreq_cooling_register(struct exynos_tmu_data *data)
 	        pr_err("gpu cooling device register fail \n");
 		return -ENODEV;
 	}
+
+#ifdef CONFIG_MALI_DEBUG_KERNEL_SYSFS
+	gpu_thermal_data = data;
+#endif
 
 	return ret;
 }
