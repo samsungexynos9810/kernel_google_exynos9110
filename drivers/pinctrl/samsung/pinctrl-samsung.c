7c471033ef0e817a903740c6699fb0b0c5ff2308
hajun.sung <hajun.sung@samsung.com>
2016-12-19 17:53:31 +0900

    [COMMON] pinctrl: samsung: Suppress unbinding
    - prevent theoretical attacks : use after free : kernel panic
    - patch reference : http://lists.infradead.org/pipermail/linux-arm-kernel/2016-May/429851.html
    
    Change-Id: I118e10be8d5570fffe3cd1ebb51d51969a367878
    Signed-off-by: hajun.sung <hajun.sung@samsung.com>

--- drivers/pinctrl/samsung/pinctrl-samsung.c
+++ drivers/pinctrl/samsung/pinctrl-samsung.c
@@ -1412,6 +1412,7 @@ static struct platform_driver samsung_pinctrl_driver = {
 	.driver = {
 		.name	= "samsung-pinctrl",
 		.of_match_table = samsung_pinctrl_dt_match,
+		.suppress_bind_attrs = true,
 	},
 };
 
