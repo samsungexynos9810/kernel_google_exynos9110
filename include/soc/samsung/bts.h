2db089ba8f872341db41d53942a11b5fde2f62a0
Jaejoon Yoo <joonyj7.yoo@samsung.com>
2017-07-25 13:55:13 +0900

    [7270] bts: add initial BTS driver
    Change-Id: If08316263f5607a9204eac17e8928fcbedf985a5
    Signed-off-by: Jaejoon Yoo <joonyj7.yoo@samsung.com>

--- include/soc/samsung/bts.h
+++ include/soc/samsung/bts.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2014 Samsung Electronics Co., Ltd.
- *		http://www.samsung.com
+ *              http://www.samsung.com
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -12,31 +12,31 @@
 #define __EXYNOS_BTS_H_
 
 #if defined(CONFIG_EXYNOS8890_BTS)
-#define CAM_FACTOR		3
-#define MIF_UTIL		50
-#define MIF_UTIL2		55
-#define MIF_DECODING		828000	// TBD
-#define MIF_ENCODING		1068000	// TBD
-#define INT_UTIL		70
-#define BUS_WIDTH		16
-#define SIZE_FACTOR(a)		a = ((a) * 16 / 10)
-#define FULLHD_SRC		1920 * 1080
+#define CAM_FACTOR              3
+#define MIF_UTIL                50
+#define MIF_UTIL2               55
+#define MIF_DECODING            828000  // TBD
+#define MIF_ENCODING            1068000 // TBD
+#define INT_UTIL                70
+#define BUS_WIDTH               16
+#define SIZE_FACTOR(a)          a = ((a) * 16 / 10)
+#define FULLHD_SRC              1920 * 1080
 #elif defined(CONFIG_EXYNOS7420_BTS) || defined(CONFIG_EXYNOS7890_BTS)
-#define DECON_NOCNT		10
-#define VPP_ROT			4
-#define CAM_FACTOR		3
-#define MIF_UTIL		50
-#define MIF_UTIL2		60
-#define MIF_UPPER_UTIL		44
-#define MIF_DECODING		828000
-#define MIF_ENCODING		1068000
-#define INT_UTIL		65
-#define NO_CNT_TH		100
-#define BUS_WIDTH		16
-#define SIZE_FACTOR(a)		a = ((a) * 16 / 10)
-#define FULLHD_SRC		1920 * 1080
+#define DECON_NOCNT             10
+#define VPP_ROT                 4
+#define CAM_FACTOR              3
+#define MIF_UTIL                50
+#define MIF_UTIL2               60
+#define MIF_UPPER_UTIL          44
+#define MIF_DECODING            828000
+#define MIF_ENCODING            1068000
+#define INT_UTIL                65
+#define NO_CNT_TH               100
+#define BUS_WIDTH               16
+#define SIZE_FACTOR(a)          a = ((a) * 16 / 10)
+#define FULLHD_SRC              1920 * 1080
 #else
-#define FULLHD_SRC		1920 * 1080
+#define FULLHD_SRC              1920 * 1080
 #endif
 
 enum bts_media_type {
@@ -65,16 +65,16 @@ enum vpp_bw_type {
 };
 
 #if defined(CONFIG_EXYNOS8890_BTS) || defined(CONFIG_EXYNOS7870_BTS) \
-	||defined(CONFIG_EXYNOS7570_BTS)
+	||defined(CONFIG_EXYNOS7570_BTS) || defined(CONFIG_EXYNOS7270_BTS)
 void exynos_update_media_scenario(enum bts_media_type media_type,
 		unsigned int bw, int bw_type);
 #else
-#define exynos_update_media_scenario(a, b, c) do {} while (0)
+define exynos_update_media_scenario(a, b, c) do {} while (0)
 #endif
 
 #if defined(CONFIG_EXYNOS7420_BTS) || defined(CONFIG_EXYNOS7890_BTS)
 void exynos7_update_media_scenario(enum bts_media_type media_type,
-		unsigned int bw, int bw_type);
+			unsigned int bw, int bw_type);
 int exynos7_update_bts_param(int target_idx, int work);
 int exynos7_bts_register_notifier(struct notifier_block *nb);
 int exynos7_bts_unregister_notifier(struct notifier_block *nb);
@@ -92,10 +92,11 @@ int exynos_bts_unregister_notifier(struct notifier_block *nb);
 #define exynos_bts_unregister_notifier(a, b) do {} while (0)
 #endif
 
-#if defined(CONFIG_EXYNOS5430_BTS) || defined(CONFIG_EXYNOS5422_BTS)	\
+#if defined(CONFIG_EXYNOS5430_BTS) || defined(CONFIG_EXYNOS5422_BTS)    \
 	|| defined(CONFIG_EXYNOS5433_BTS)|| defined(CONFIG_EXYNOS7420_BTS) \
-	|| defined(CONFIG_EXYNOS7890_BTS) || defined(CONFIG_EXYNOS8890_BTS) \
-	|| defined(CONFIG_EXYNOS7870_BTS) || defined(CONFIG_EXYNOS7570_BTS)
+|| defined(CONFIG_EXYNOS7890_BTS) || defined(CONFIG_EXYNOS8890_BTS) \
+|| defined(CONFIG_EXYNOS7870_BTS) || defined(CONFIG_EXYNOS7570_BTS) \
+|| defined(CONFIG_EXYNOS7270_BTS)
 void bts_initialize(const char *pd_name, bool on);
 #else
 #define bts_initialize(a, b) do {} while (0)
@@ -119,10 +120,10 @@ void bts_otf_initialize(unsigned int id, bool on);
 #define bts_otf_initialize(a, b) do {} while (0)
 #endif
 
-#if defined(CONFIG_EXYNOS5422_BTS) || defined(CONFIG_EXYNOS5433_BTS)	\
+#if defined(CONFIG_EXYNOS5422_BTS) || defined(CONFIG_EXYNOS5433_BTS)    \
 	|| defined(CONFIG_EXYNOS7420_BTS) || defined(CONFIG_EXYNOS7890_BTS) \
-	|| defined(CONFIG_EXYNOS8890_BTS) || defined(CONFIG_EXYNOS7870_BTS) \
-	|| defined(CONFIG_EXYNOS7570_BTS)
+|| defined(CONFIG_EXYNOS8890_BTS) || defined(CONFIG_EXYNOS7870_BTS) \
+|| defined(CONFIG_EXYNOS7570_BTS) || defined(CONFIG_EXYNOS7270_BTS)
 enum bts_scen_type {
 	TYPE_MFC_UD_ENCODING = 0,
 	TYPE_MFC_UD_DECODING,
