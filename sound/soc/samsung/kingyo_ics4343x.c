1603d7392dc0c3dffbe31537485afddae75f1cd9
Keiichi SAKURAI <keisak@casio.co.jp>
2017-11-10 15:12:02 +0900

    AUDIO MACHINE DRIVER: In order to support two type of structure, The two structure is with/without audio mixer driver For using audio mixer, we will have to add a record route path explicitly that we do not yet.
    Change-Id: I020e1c3e008f6198a3b6e27300af5717686ca5b9
    Signed-off-by: Keiichi SAKURAI <keisak@casio.co.jp>

--- sound/soc/samsung/kingyo_ics4343x.c
+++ sound/soc/samsung/kingyo_ics4343x.c
@@ -23,57 +23,70 @@
 #include "i2s.h"
 #include "i2s-regs.h"
 
-#if 1
+#ifdef CONFIG_SND_SOC_SAMSUNG_VERBOSE_DEBUG
 	#define gprintk(fmt, x... ) printk( "%s: " fmt, __FUNCTION__ , ## x)
 #else
 	#define gprintk(x...) do { } while (0)
 #endif
 
 /*
- * TODO: Do we need register Audio component to control it.
- * If we do not use its function, we should remove this.
+ * TODO: we should bind "Samsung mixer component" to control its registers
+ * however we must do a lot of things to do by using kcontrol,
+ * e.g. define Widget, route, etc.
+ * Kingyo must just initialize mixer only for recording so we had better
+ * simply write registers with static configuration data.
+ * Namely undefine the following pre-processor value.
  */
-#define MIXER_COMPONENT
+#ifdef CONFIG_SND_SAMSUNG_MIXER_COMPONENT
 /*
- * We must set divider of mixer clock
+ * define this if you use dapm and calling snd_soc_dapm_sync is neccessary.
  */
-#define MIXER_SET_CLKDIV
+#define CALL_DAPM_SYNC
+
+#else
+/*
+ * If We must set divider of mixer clock for each when hw_setting
+ */
+#define SET_MIXER_CLK_AT_STREAM
+
 /*
  * We should use clock api rather than this iomapping method,
- * so we must change it if they would support the clock
+ * so we must change it if it would work correctly.
  */
 #define MIXER_CLK_IOREGMAP
+
+#define AUDMIXER_SYS_CLK_FREQ_48KHZ	(24576000U + 100)
+
+#endif
+
 /*
- * We must configire mixer register here until audio mixer driver
- * will be supported.
+ * define this, if we use dts node to define cpu/codec dai name
  */
-#define MIXER_CONTORL_IOREG
+//#define DAI_DEFINE_BY_DTS
 
 /*
  * The following paramters are set in bootloader. These paramters are no longer
  * used here, they are accessed by audio_fw.c in platform HAL layser throgh
  * this machine driver layer.
  */
-int MicGain = 1;
+
+ int MicGain = 1;
 int IDVol = 16;
 module_param(MicGain, int, S_IRUGO|S_IWUSR);
 module_param(IDVol, int, S_IRUGO|S_IWUSR);
 
-
+#ifndef CONFIG_SND_SAMSUNG_MIXER_COMPONENT
 #ifdef MIXER_CLK_IOREGMAP
 static void __iomem *dispaud_vclk;
-// #define CMU_DISPAUD_REG(x)     (disaud_vclk + (x))
 #define PA_CMU_DISPAUD				0x148D0000
 #define CLK_CON_DIV_CLK_DISPAUD_MIXER 0x410
 #endif
-#ifdef MIXER_CONTORL_IOREG
 #include "../codecs/exynos-audmixer-regs.h"
 static void __iomem *audmixer_reg;
 #define MIXER_AUD					0x14880000
 #endif
 
-
-#ifdef MIXER_COMPONENT
+#ifdef CONFIG_SND_SAMSUNG_MIXER_COMPONENT
 static const struct snd_soc_component_driver smdk7270_cmpnt = {
 	.name = "smdk7270-audio",
 };
@@ -120,8 +133,8 @@ static void kingyo_aif_shutdown(struct snd_pcm_substream *substream)
 
 }
 
-#ifdef MIXER_SET_CLKDIV
-static int set_mixer_div(void)
+#ifndef CONFIG_SND_SAMSUNG_MIXER_COMPONENT
+static int set_mixer_clock(struct snd_soc_card *card)
 {
 #ifdef MIXER_CLK_IOREGMAP
 	if (dispaud_vclk == NULL) {
@@ -133,21 +146,19 @@ static int set_mixer_div(void)
 	}
 	writel(0x3, dispaud_vclk + CLK_CON_DIV_CLK_DISPAUD_MIXER);
 #else
-	div_mixer = devm_clk_get(dev, "div_audmixer");
+	struct clk *div_mixer = devm_clk_get(card->dev, "audmixer_dout");
 	if (IS_ERR(div_mixer)) {
-		gprintk("clk_get div_mixer return error\n");
+		gprintk("audmixer_dout clk not found\n");
 		return -ENOMEM;
 	}
-	if ( clk_set_rate(div_mixer, 3) ) {
-		gprintk("clk_set div_mixer return error\n");
+	if ( clk_set_rate(div_mixer, AUDMIXER_SYS_CLK_FREQ_48KHZ) ) {
+		gprintk("clk_set audmixer_dout return error\n");
 	}
-	devm_clk_put(dev,div_mixer);
+	devm_clk_put(card->dev,div_mixer);
 #endif
 	return 0;
 }
-#endif
 
-#ifdef MIXER_CONTORL_IOREG
 static unsigned int mixer_config_regs[] = {
 	AUDMIXER_REG_10_DMIX1, 0x00,
 	AUDMIXER_REG_11_DMIX2, 0x80,
@@ -173,40 +184,66 @@ static int config_audmixer(void)
 	return 0;
 }
 #endif
+
 /*
  * kingyo hw params. (AP I2S Master with mic)
  */
 static int kingyo_hw_params(struct snd_pcm_substream *substream,
 	struct snd_pcm_hw_params *params)
 {
-
-	gprintk("\n");
-
-	/* Do we need call here again as another module overwrite this register */
-	#ifdef MIXER_SET_CLKDIV
-	set_mixer_div();
-	#endif
-
-	/* Do we need to call sysclock for i2s here */
-	{
 	struct snd_soc_pcm_runtime *rtd = substream->private_data;
 	struct snd_soc_dai *cpu_dai = rtd->cpu_dai;
 	int rfs=256, bfs=64;
+	int ret = 0;
+	gprintk("fs=%u\n", params_rate(params));
+
+	/* Do we need call here again as another module overwrite this register */
+	#ifdef SET_MIXER_CLK_AT_STREAM
+	set_mixer_clock(rtd->card);
+	#endif
+
+	/* We need to call sysclock for i2s here */
 	snd_soc_dai_set_sysclk(cpu_dai, SAMSUNG_I2S_OPCLK, 0, MOD_OPCLK_PCLK);
 	snd_soc_dai_set_sysclk(cpu_dai, SAMSUNG_I2S_CDCLK, rfs, SND_SOC_CLOCK_OUT);
 	snd_soc_dai_set_sysclk(cpu_dai, SAMSUNG_I2S_RCLKSRC_1, 0, 0);
 	snd_soc_dai_set_clkdiv(cpu_dai, SAMSUNG_I2S_DIV_BCLK, bfs);
 	snd_soc_dai_set_clkdiv(cpu_dai, SAMSUNG_I2S_DIV_RCLK, rfs);
-	}
 
 	/* The following setting mixer register might be removed after
 	 * mixer driver funcions would be supported */
-	#ifdef MIXER_CONTORL_IOREG
-	config_audmixer();
+	#ifdef CONFIG_SND_SAMSUNG_MIXER_COMPONENT
+	{
+		struct snd_soc_card *card = rtd->card;
+		struct snd_soc_dai *amixer_dai = rtd->codec_dais[0];
+		#if 1
+		struct snd_soc_dai *dai = amixer_dai;
+		int ratio = bfs;
+			if (dai->driver && dai->driver->ops->set_bclk_ratio) {
+				ret = dai->driver->ops->set_bclk_ratio(dai, ratio);
+				if ( ret != 0) {
+					gprintk("result error %d\n",ret);
+				}
+			} else {
+				if (!dai->driver)
+					gprintk("parameter driver error\n");
+				else
+					gprintk("parameter ratio error\n");
+				return -EINVAL;
+			}
+		#else
+		ret = snd_soc_dai_set_bclk_ratio(amixer_dai, bfs);
+		#endif
+		if (ret < 0) {
+			dev_err(card->dev, "aif1: Failed to configure mixer\n");
+			return ret;
+		}
+	}
+	#else
+		ret = config_audmixer();
 	#endif
 
 	gprintk("exit\n");
-	return 0;
+	return ret;
 }
 
 
@@ -216,6 +253,7 @@ static struct snd_soc_ops kingyo_ops = {
 	.hw_params = kingyo_hw_params,
 };
 
+#ifdef CALL_DAPM_SYNC
 static int kingyo_ics434_init(struct snd_soc_pcm_runtime *rtd)
 {
 	struct snd_soc_dapm_context *dapm = snd_soc_codec_get_dapm(rtd->codec);
@@ -225,20 +263,43 @@ static int kingyo_ics434_init(struct snd_soc_pcm_runtime *rtd)
 
 	return 0;
 }
+#endif
+
+#ifdef CONFIG_SND_SAMSUNG_MIXER_COMPONENT
+static struct snd_soc_dai_link_component codecs_ap0[] = {{
+		.name = "14880000.s1403x",
+		.dai_name = "AP0",
+	}, {
+		.name = "ics43432",
+		.dai_name = "ics43432-hifi",
+	},
+};
+#endif
 
 /*
+ * FE DAI links is just only for Capture by MiC
  * dai_fmt: configure initial format to i2s driver
  */
 static struct snd_soc_dai_link kingyo_dai[] = {
+	/* Recording mic */
 	{
-		.name = "ICS43432 HiFi",
+		.name = "ICS43432 Direct",
 		.stream_name = "Capture",
-		.codec_dai_name = "ics43432-hifi",
+#ifdef CONFIG_SND_SAMSUNG_MIXER_COMPONENT
+		.codecs = codecs_ap0,
+		.num_codecs = ARRAY_SIZE(codecs_ap0),
+#else
 		.codec_name = "ics43432",
+		.codec_dai_name = "ics43432-hifi",
+#endif
+#ifdef CALL_DAPM_SYNC
 		.init = kingyo_ics434_init,
-		.ops = &kingyo_ops,
+#endif
+#ifndef CPU_DAI_BY_DTS
 		.cpu_dai_name = "148a0000.i2s",
 		.platform_name = "148a0000.i2s",
+#endif
+		.ops = &kingyo_ops,
 		.dai_fmt = SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_CBS_CFS | SND_SOC_DAIFMT_NB_NF,
 	},
 };
@@ -254,15 +315,13 @@ static int kingyo_probe(struct snd_soc_card *card) {
 		return -ENOMEM;
 	}
 
-	#ifdef MIXER_SET_CLKDIV
+#ifndef CONFIG_SND_SAMSUNG_MIXER_COMPONENT
 	/* set CLK_DISPAUD_MIXER DIV_RATIO */
-	set_mixer_div();
-	#endif
+	set_mixer_clock(card);
 
-	#ifdef MIXER_CONTORL_IOREG
 	/* iomapping audio mixer */
 	map_audiomixer();
-	#endif
+#endif
 
 	return 0;
 }
@@ -272,6 +331,26 @@ static int kingyo_remove(struct snd_soc_card *card) {
 	return 0;
 }
 
+#ifdef CONFIG_SND_SAMSUNG_MIXER_COMPONENT
+static int audmixer_init(struct snd_soc_component *cmp)
+{
+	gprintk("\n");
+	return 0;
+}
+
+static struct snd_soc_aux_dev audmixer_aux_dev[] = {
+	{
+		.name = "S1403X",
+		.init = audmixer_init,
+	},
+};
+
+static struct snd_soc_codec_conf audmixer_codec_conf[] = {
+	{
+		.name_prefix = "AudioMixer",
+	},
+};
+#endif
 
 static struct snd_soc_card kingyo = {
 	.name = "KINGYO-ICS4343X",
@@ -279,19 +358,32 @@ static struct snd_soc_card kingyo = {
 	.remove = kingyo_remove,
 	.dai_link = kingyo_dai,
 	.num_links = ARRAY_SIZE(kingyo_dai),
+#ifdef CONFIG_SND_SAMSUNG_MIXER_COMPONENT
+	.aux_dev = audmixer_aux_dev,
+	.num_aux_devs = ARRAY_SIZE(audmixer_aux_dev),
+	.codec_conf = audmixer_codec_conf,
+	.num_configs = ARRAY_SIZE(audmixer_codec_conf),
+#endif
 };
 
 static int kingyo_ics4343X_probe(struct platform_device *pdev)
 {
-	int ret;
+	struct device_node *np = pdev->dev.of_node;
 	struct snd_soc_card *card = &kingyo;
+	int ret;
 
-	gprintk("start \n");
-	/* register card */
+	gprintk("\n");
+	if (!np) {
+		dev_err(&pdev->dev, "Failed to get device node\n");
+		return -EINVAL;
+	}
+
 	card->dev = &pdev->dev;
 
-	#ifdef MIXER_COMPONENT
+	#ifdef CONFIG_SND_SAMSUNG_MIXER_COMPONENT
 	{
+		int n;
+		struct device_node *auxdev_np;
 		gprintk("register_component\n");
 		ret = snd_soc_register_component(&pdev->dev, &smdk7270_cmpnt,
 				smdk7270_ext_dai,	0);
@@ -299,9 +391,65 @@ static int kingyo_ics4343X_probe(struct platform_device *pdev)
 			dev_err(&pdev->dev, "Failed to register component: %d\n", ret);
 			return ret;
 		}
+
+		for (n = 0; n < ARRAY_SIZE(audmixer_aux_dev); n++) {
+			auxdev_np = of_parse_phandle(np, "samsung,auxdev", n);
+			if (!auxdev_np) {
+				dev_err(&pdev->dev,
+					"Property 'samsung,auxdev' missing\n");
+				return -EINVAL;
+			}
+			gprintk("auxdev[%d] of_node\n",n);
+			#ifdef AUX_DEV_DEFINE
+			audmixer_aux_dev[n].codec_of_node = auxdev_np;
+			#endif
+			audmixer_codec_conf[n].of_node = auxdev_np;
+		}
 	}
 	#endif
 
+#ifdef DAI_DEFINE_BY_DTS
+	{
+		struct device_node *cpu_np, *codec_np;
+
+		gprintk("cpu and codecs of_node\n");
+		for (n = 0; n < ARRAY_SIZE(kingyo_dai); n++) {
+			/* Skip parsing DT for fully formed dai links */
+			if (kingyo_dai[n].platform_name && kingyo_dai[n].codec_name) {
+				dev_dbg(card->dev,
+				"Skipping dt for populated dai link %s\n",
+				kingyo_dai[n].name);
+				continue;
+			}
+
+			codec_np = of_parse_phandle(np, "samsung,audio-codec", n);
+			if (!codec_np) {
+				dev_err(&pdev->dev,	"Property 'samsung,audio-codec' missing\n");
+				break;
+			}
+			#ifdef CONFIG_SND_SAMSUNG_MIXER_COMPONENT
+			kingyo_dai[n].codecs[1].of_node = codec_np;
+			#endif
+
+
+			cpu_np = of_parse_phandle(np, "samsung,audio-cpu", n);
+			if (!cpu_np) {
+				dev_err(&pdev->dev,	"Property 'samsung,audio-cpu' missing\n");
+				break;
+			}
+
+			if (!kingyo_dai[n].cpu_dai_name)
+				kingyo_dai[n].cpu_of_node = cpu_np;
+			if (!kingyo_dai[n].platform_name)
+				kingyo_dai[n].platform_of_node = cpu_np;
+			if (!kingyo_dai[n].codec_dai_name)
+				kingyo_dai[n].codec_of_node = cpu_np;
+
+		}
+	}
+#endif
+
+	gprintk("register card\n");
 	ret = snd_soc_register_card(card);
 	if (ret) {
 		dev_err(&pdev->dev, "snd_soc_register_card() failed: %d\n", ret);
@@ -341,6 +489,5 @@ static struct platform_driver kingyo_ics4343X_driver = {
 };
 module_platform_driver(kingyo_ics4343X_driver);
 
-MODULE_AUTHOR("keisak <keisak@casio.co.jp>");
 MODULE_DESCRIPTION("ALSA SoC Driver for Kingyo Board");
 MODULE_LICENSE("GPL");
