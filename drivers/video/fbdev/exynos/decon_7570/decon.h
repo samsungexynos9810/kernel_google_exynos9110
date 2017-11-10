4c6f372b44407f382d9227981afab5f6a4a61b55
Kim Manseok <manseoks.kim@samsung.com>
2017-11-10 15:55:54 +0900

    fbdev: decon_7570: keep the lcd reset gpio state to high after sleep
    Change-Id: I4088e9c2ac85f6f3eb54242ddac0bf70183b8a74
    Signed-off-by: Kim Manseok <manseoks.kim@samsung.com>

--- drivers/video/fbdev/exynos/decon_7570/decon.h
+++ drivers/video/fbdev/exynos/decon_7570/decon.h
@@ -35,7 +35,7 @@ extern struct ion_device *ion_exynos;
 extern struct decon_device *decon_int_drvdata;
 extern int decon_log_level;
 
-#define BRINGUP_DEBUG
+//#define BRINGUP_DEBUG
 
 #if defined(CONFIG_ARM_EXYNOS7570_BUS_DEVFREQ)
 #define CONFIG_DECON_DEVFREQ
