0df71e6f00bdc23ccedceb4679b3eedbfd7fe20d
Shohei Sakamoto <sakamotos@casio.co.jp>
2018-07-11 10:58:05 +0900

    misc: casio: Add gps devices
    Change-Id: I20f49fab4b4f2711737b68479c23fe47b40f987f
    Signed-off-by: Shohei Sakamoto <sakamotos@casio.co.jp>

--- drivers/misc/casio/board-kingyo-gps.c
+++ drivers/misc/casio/board-kingyo-gps.c
@@ -29,6 +29,9 @@
 #include <linux/of_platform.h>
 #include <linux/of_gpio.h>
 #include <linux/of_irq.h>
+#include <linux/cdev.h>
+#include <linux/poll.h>
+#include <asm/uaccess.h>
 
 struct gps_info {
 	int nr_gpio;
@@ -60,12 +63,304 @@ static irqreturn_t host_wake_isr(int irq, void *dev)
 	return IRQ_HANDLED;
 }
 
+#define BUF0_SIZE	2048
+#define BUF1_SIZE	32
+
+struct gps_buffer {
+	int ridx0;
+	int widx0;
+	wait_queue_head_t wait0;
+	spinlock_t slock0;
+	__u8 buf0[BUF0_SIZE];
+
+	int ridx1;
+	int widx1;
+	wait_queue_head_t wait1;
+	spinlock_t slock1;
+	__u8 buf1[BUF1_SIZE];
+};
+
+static struct gps_buffer *gb;
+
+/* called from assistservice to write to buf0 */
+static ssize_t gps0_write(struct file *file, const char *buf,
+				size_t count, loff_t *offset)
+{
+	int num, widx, tmp, ret, copied = 0;
+	unsigned long flags;
+
+	while (count) {
+		num = BUF0_SIZE - (gb->widx0 - gb->ridx0);
+		if (num) {
+			if (num > count)
+				num = count;
+			widx = gb->widx0 & (BUF0_SIZE - 1);
+			tmp = BUF0_SIZE - widx;
+			if (tmp >= num) {
+				if (copy_from_user(&gb->buf0[widx], buf, num))
+					return -EFAULT;
+			} else {
+				if (copy_from_user(&gb->buf0[widx], buf, tmp))
+					return -EFAULT;
+				if (copy_from_user(&gb->buf0[0], buf + tmp, num - tmp))
+					return -EFAULT;
+			}
+			buf += num;
+			spin_lock_irqsave(&gb->slock0, flags);
+			gb->widx0 += num;
+			spin_unlock_irqrestore(&gb->slock0, flags);
+			wake_up_interruptible(&gb->wait0);
+			count -= num;
+			copied += num;
+		} else {
+			ret = wait_event_interruptible(gb->wait0, BUF0_SIZE - (gb->widx0 - gb->ridx0));
+			if (ret < 0)
+				return ret;
+		}
+	}
+	return copied;
+}
+
+/* called from gps_hal to read from buf0 */
+static ssize_t gps1_read(struct file *file, char *buf, size_t count,
+				loff_t *offset)
+{
+	int num, ridx, tmp, ret;
+	unsigned long flags;
+
+	if (gb->widx0 > 32768 && gb->ridx0 > 32768) {
+		spin_lock_irqsave(&gb->slock0, flags);
+		gb->widx0 -= 32768;
+		gb->ridx0 -= 32768;
+		spin_unlock_irqrestore(&gb->slock0, flags);
+	}
+	num = gb->widx0 - gb->ridx0;
+	if (num == 0) {	/* empty */
+		if (file->f_flags & O_NONBLOCK)
+			return -EAGAIN;
+		ret = wait_event_interruptible(gb->wait0, gb->widx0 - gb->ridx0);
+		if (ret < 0)
+			return ret;
+		num = gb->widx0 - gb->ridx0;
+	}
+	if (num > count)
+		num = count;
+	ridx = gb->ridx0 & (BUF0_SIZE - 1);
+	tmp = BUF0_SIZE - ridx;
+	if (tmp >= num) {
+		if (copy_to_user(buf, &gb->buf0[ridx], num))
+			return -EFAULT;
+	} else {
+		if (copy_to_user(buf, &gb->buf0[ridx], tmp))
+			return -EFAULT;
+		if (copy_to_user(buf + tmp, &gb->buf0[0], num - tmp))
+			return -EFAULT;
+	}
+	spin_lock_irqsave(&gb->slock0, flags);
+	gb->ridx0 += num;
+	spin_unlock_irqrestore(&gb->slock0, flags);
+	wake_up_interruptible(&gb->wait0);
+	return num;
+}
+
+/* called from gps_hal to write to buf1 */
+static ssize_t gps1_write(struct file *file, const char *buf,
+				size_t count, loff_t *offset)
+{
+	int num, widx, tmp, ret;
+	unsigned long flags;
+
+	num = BUF1_SIZE - (gb->widx1 - gb->ridx1);
+	if (num == 0) {	/* no room */
+		if (file->f_flags & O_NONBLOCK)
+			return -EAGAIN;
+		ret = wait_event_interruptible(gb->wait1, BUF1_SIZE - (gb->widx1 - gb->ridx1));
+		if (ret < 0)
+			return ret;
+		num = BUF1_SIZE - (gb->widx1 - gb->ridx1);
+	}
+	if (num > count)
+		num = count;
+	widx = gb->widx1 & (BUF1_SIZE - 1);
+	tmp = BUF1_SIZE - widx;
+	if (tmp >= num) {
+		if (copy_from_user(&gb->buf1[widx], buf, num))
+			return -EFAULT;
+	} else {
+		if (copy_from_user(&gb->buf1[widx], buf, tmp))
+			return -EFAULT;
+		if (copy_from_user(&gb->buf1[0], buf + tmp, num - tmp))
+			return -EFAULT;
+	}
+	spin_lock_irqsave(&gb->slock1, flags);
+	gb->widx1 += num;
+	spin_unlock_irqrestore(&gb->slock1, flags);
+	wake_up_interruptible(&gb->wait1);
+	return num;
+}
+
+/* called from assistservice to read from buf1 */
+static ssize_t gps0_read(struct file *file, char *buf, size_t count,
+				loff_t *offset)
+{
+	int num, ridx, tmp, ret, copied = 0;
+	unsigned long flags;
+
+	if (gb->widx1 > 32768 && gb->ridx1 > 32768) {
+		spin_lock_irqsave(&gb->slock1, flags);
+		gb->widx1 -= 32768;
+		gb->ridx1 -= 32768;
+		spin_unlock_irqrestore(&gb->slock1, flags);
+	}
+	while (count) {
+		num = gb->widx1 - gb->ridx1;
+		if (num) {
+			if (num > count)
+				num = count;
+			ridx = gb->ridx1 & (BUF1_SIZE - 1);
+			tmp = BUF1_SIZE - ridx;
+			if (tmp >= num) {
+				if (copy_to_user(buf, &gb->buf1[ridx], num))
+					return -EFAULT;
+			} else {
+				if (copy_to_user(buf, &gb->buf1[ridx], tmp))
+					return -EFAULT;
+				if (copy_to_user(buf + tmp, &gb->buf1[0], num - tmp))
+					return -EFAULT;
+			}
+			buf += num;
+			spin_lock_irqsave(&gb->slock1, flags);
+			gb->ridx1 += num;
+			spin_unlock_irqrestore(&gb->slock1, flags);
+			wake_up_interruptible(&gb->wait1);
+			count -= num;
+			copied += num;
+		} else {
+			ret = wait_event_interruptible(gb->wait1, gb->widx1 - gb->ridx1);
+			if (ret < 0)
+				return ret;
+		}
+	}
+	return copied;
+}
+
+static unsigned int gps1_poll(struct file *file, poll_table *wait)
+{
+	int num;
+	unsigned int mask = 0;
+
+	poll_wait(file, &gb->wait0, wait);
+	poll_wait(file, &gb->wait1, wait);
+
+	num = gb->widx0 - gb->ridx0;
+	if (num)
+		mask |= POLLIN | POLLRDNORM;
+	num = BUF1_SIZE - (gb->widx1 - gb->ridx1);
+	if (num)
+		mask |= POLLOUT | POLLWRNORM;
+
+	return mask;
+}
+
+static int gps0_open(struct inode *inode, struct file *file)
+{
+	return 0;
+}
+
+static int gps1_open(struct inode *inode, struct file *file)
+{
+	return 0;
+}
+
+static int gps0_close(struct inode *inode, struct file *file)
+{
+	return 0;
+}
+
+static int gps1_close(struct inode *inode, struct file *file)
+{
+	return 0;
+}
+
+static const struct file_operations gps1_fops = {
+	.owner = THIS_MODULE,
+	.open = gps1_open,
+	.release = gps1_close,
+	.read = gps1_read,
+	.write = gps1_write,
+	.poll = gps1_poll,
+};
+
+static const struct file_operations gps0_fops = {
+	.owner = THIS_MODULE,
+	.open = gps0_open,
+	.release = gps0_close,
+	.read = gps0_read,
+	.write = gps0_write,
+};
+
+static struct class *gps_class;
+static struct cdev gps_cdev0, gps_cdev1;
+
 static int kingyo_gps_probe(struct platform_device *pdev)
 {
 	int ret, irq;
 	struct gps_info *info;
+	dev_t devno;
+	struct device *device;
 
 	pr_info("%s\n", __func__);
+
+	ret = alloc_chrdev_region(&devno, 0, 2, "kingyo_gps");
+	if (ret < 0) {
+		pr_err("%s: alloc_chrdev_region failed %d\n", __func__, ret);
+		return ret;
+	}
+	gps_class = class_create(THIS_MODULE, "kingyo_gps");
+	if (IS_ERR(gps_class)) {
+		ret = PTR_ERR(gps_class);
+		pr_err("%s: class_create failed %d\n", __func__, ret);
+		return ret;
+	}
+
+	/* gps0 */
+	cdev_init(&gps_cdev0, &gps0_fops);
+	gps_cdev0.owner = THIS_MODULE;
+	ret = cdev_add(&gps_cdev0, MKDEV(MAJOR(devno),0), 1);
+	if (ret < 0) {
+		pr_err("%s: cdev_add failed %d\n", __func__, ret);
+		return ret;
+	}
+	device = device_create(gps_class, NULL, MKDEV(MAJOR(devno),0), NULL, "gps0");
+	if (IS_ERR(device)) {
+		ret = PTR_ERR(device);
+		pr_err("%s: device_create failed %d\n", __func__, ret);
+		return ret;
+	}
+	/* gps1 */
+	cdev_init(&gps_cdev1, &gps1_fops);
+	gps_cdev1.owner = THIS_MODULE;
+	ret = cdev_add(&gps_cdev1, MKDEV(MAJOR(devno),1), 1);
+	if (ret < 0) {
+		pr_err("%s: cdev_add failed %d\n", __func__, ret);
+		return ret;
+	}
+	device = device_create(gps_class, NULL, MKDEV(MAJOR(devno),1), NULL, "gps1");
+	if (IS_ERR(device)) {
+		ret = PTR_ERR(device);
+		pr_err("%s: device_create failed %d\n", __func__, ret);
+		return ret;
+	}
+	gb = devm_kzalloc(&pdev->dev, sizeof(struct gps_buffer), GFP_KERNEL);
+	if (!gb)
+		return -ENOMEM;
+
+	init_waitqueue_head(&gb->wait0);
+	init_waitqueue_head(&gb->wait1);
+	spin_lock_init(&gb->slock0);
+	spin_lock_init(&gb->slock1);
+
+	/* host_wake */
 	info = devm_kzalloc(&pdev->dev, sizeof(struct gps_info), GFP_KERNEL);
 	if (!info)
 		return -ENOMEM;
