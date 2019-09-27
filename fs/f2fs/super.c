e119703e31afe31bfa9aefe3830296e97b046015
Chengguang Xu via Linux-f2fs-devel <linux-f2fs-devel@lists.sourceforge.net>
2019-09-27 09:35:48 +0800

    f2fs: mark recovery flag correctly in read_raw_super_block()
    On the combination of first fail and second success,
    we will miss to mark recovery flag because currently
    we reuse err variable in the loop.
    
    Signed-off-by: Chengguang Xu <cgxu519@zoho.com.cn>
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -2875,6 +2875,7 @@ static int read_raw_super_block(struct f2fs_sb_info *sbi,
 			f2fs_err(sbi, "Unable to read %dth superblock",
 				 block + 1);
 			err = -EIO;
+			*recovery = 1;
 			continue;
 		}
 
@@ -2884,6 +2885,7 @@ static int read_raw_super_block(struct f2fs_sb_info *sbi,
 			f2fs_err(sbi, "Can't find valid F2FS filesystem in %dth superblock",
 				 block + 1);
 			brelse(bh);
+			*recovery = 1;
 			continue;
 		}
 
@@ -2895,10 +2897,6 @@ static int read_raw_super_block(struct f2fs_sb_info *sbi,
 		}
 		brelse(bh);
 	}
-
-	/* Fail to read any one of the superblocks*/
-	if (err < 0)
-		*recovery = 1;
 
 	/* No valid superblock */
 	if (!*raw_super)
